e68e01f280c371f5485c40f440228c236cc5bf29
Antonio Serrano Muñoz <aserrano@mondragon.edu>
2021-07-14 23:53:11 +0200

    Create the base model using torch

--- models/torch/base.py
+++ models/torch/base.py
@@ -2,9 +2,10 @@ import gym
 import torch
 import torch.nn as nn
 
-class BaseModel(nn.Module):
+
+class Model(nn.Module):
     def __init__(self) -> None:
-        super(BaseModel, self).__init__()
+        super(Model, self).__init__()
         self.network = None
         
     def forward(self):
@@ -20,6 +21,7 @@ class BaseModel(nn.Module):
         self.network.load_state_dict(torch.load(path))
     
     def activation_funtion_by_name(self, name):
+        # TODO: increase the list
         if name == "elu":
             return nn.ELU()
         elif name == "selu":
@@ -34,13 +36,14 @@ class BaseModel(nn.Module):
             return nn.Sigmoid()
         return None
 
-    def built_network_from_cfg(self, cfg: dict, obs_space: gym.spaces.Space, action_space: gym.spaces.Space):
+    def built_network_from_cfg(self, cfg: dict, state_space: gym.spaces.Space, action_space: gym.spaces.Space):
+        # TODO: add output activation function
         layers = []
-        hidden_layers = cfg.get("hidden_layers", default=[256, 256])
-        activation = activation_funtion_by_name(cfg.get("hidden_activation", "relu"))
+        hidden_layers = cfg.get("hidden_layers", [256, 256])
+        activation = self.activation_funtion_by_name(cfg.get("hidden_activation", "relu"))
 
         # first layer
-        layers.append(nn.Linear(*obs_space.shape, hidden_layers[0]))
+        layers.append(nn.Linear(*state_space.shape, hidden_layers[0]))
         layers.append(activation)
 
         # add remaining layers

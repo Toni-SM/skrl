ecd25bb8e9c088004a7693d9d3a214bedc95b678
Antonio Serrano Muñoz <aserrano@mondragon.edu>
2021-07-17 20:02:43 +0200

    Storage single and multiple transtitions all at once

--- memory/base.py
+++ memory/base.py
@@ -1,10 +1,43 @@
+from typing import Union
+
 import torch
 from gym import spaces
 
 
 class Memory:
-    def __init__(self, buffer_size: int, num_envs: int = 1, device: str = "cuda:0", preallocate: bool = True, state_space=None, action_space=None) -> None:
+    def __init__(self, buffer_size: int, num_envs: int = 1, device: str = "cuda:0", preallocate: bool = True, state_space: Union[spaces.Space, None] = None, action_space: Union[spaces.Space, None] = None) -> None:
+        """
+        Base class that represent memory with circular buffers
+
+        The implementation creates the buffers with the following shapes if the preallocation flag is set to true
+        - states (buffer_size, num_envs, *state_space.shape)
+        - actions (buffer_size, num_envs, *action_space.shape
+        - rewards (buffer_size, num_envs, 1)
+        - next_states (buffer_size, num_envs, *state_space.shape)
+        - dones (buffer_size, num_envs, 1)
+
+        Parameters
+        ----------
+        buffer_size: int
+            Maximum number of items in the buffer 
+        num_envs: int
+            Number of parallel environments
+        device: str
+            Device on which a PyTorch tensor is or will be allocated
+        preallocate: bool
+            If true, preallocate memory for efficient use
+        state_space: Union[gym.spaces.Space, None]
+            State/observation space
+        action_space: Union[gym.spaces.Space, None]
+            Action space
+        
+        Returns
+        -------
+        bool
+            PASS
+        """
         # TODO: handle dynamic memory
+        # TODO: show memory consumption
 
         if preallocate:
             if not isinstance(state_space, spaces.Space):
@@ -17,45 +50,91 @@ class Memory:
         self.num_envs = num_envs
         self.device = device
 
-        self.position = 0
         self.filled = False
+        self.position_env = 0
+        self.position_buffer = 0
 
+        # buffers
         self.states = torch.zeros(buffer_size, num_envs, *state_space.shape, device=self.device)
         self.actions = torch.zeros(buffer_size, num_envs, *action_space.shape, device=self.device)
         self.rewards = torch.zeros(buffer_size, num_envs, 1, device=self.device)
         self.next_states = torch.zeros(buffer_size, num_envs, *state_space.shape, device=self.device)
         self.dones = torch.zeros(buffer_size, num_envs, 1, device=self.device).byte()
 
-    def __len__(self):
-        return self.buffer_size
+        # alternative names
+        self.push = self.add_transitions
+        self.add_transition = self.add_transitions
 
-    def add_transition(self, state, action, reward, next_state, done):
-        if self.position >= self.buffer_size:
-            self.position = 0
+    def __len__(self):
+        """
+        Current (valid) size of the buffer according to the first dimension (buffer_size)
+        """
+        return self.buffer_size if self.filled else self.position_buffer
+
+    def reset(self) -> None:
+        """
+        Reset the memory
+        """
+        self.filled = False
+        self.position_env = 0
+        self.position_buffer = 0
+
+    def add_transitions(self, states: torch.Tensor, actions: torch.Tensor, rewards: torch.Tensor, next_states: torch.Tensor, dones: torch.Tensor) -> None:
+        """
+        Record a single transition or a transition batch in the memory
+
+        Single transition:
+            Storage a single transition (tensors with one dimension) and increment the position of the environment pointer (second index)
+        Transition batch:
+            Store the transitions (where tensors' first dimension must be equal to the number of parallel environments) and increment the position of the buffer pointer (first index)
+
+        Parameters
+        ----------
+        states: torch.Tensor
+            States/observations of the environment used to make the decision
+        actions: torch.Tensor
+            Actions taken by the agent
+        rewards: torch.Tensor
+            Instant rewards achieved by the current actions
+        next_states: torch.Tensor
+            Next states/observations of the environment
+        dones: torch.Tensor
+            Signals to indicate that episodes have ended
+        """
+        # single env transition
+        if states.dim() == 1:
+            self.states[self.position_buffer, self.position_env].copy_(states)
+            self.actions[self.position_buffer, self.position_env].copy_(actions)
+            self.rewards[self.position_buffer, self.position_env].copy_(rewards.view(-1))
+            self.next_states[self.position_buffer, self.position_env].copy_(next_states)
+            self.dones[self.position_buffer, self.position_env].copy_(dones.view(-1))
+            self.position_env += 1
+        # multi envs transitions
+        elif states.dim() > 1 and states.shape[0] == self.num_envs:
+            self.states[self.position_buffer].copy_(states)
+            self.actions[self.position_buffer].copy_(actions)
+            self.rewards[self.position_buffer].copy_(rewards.view(-1, 1))
+            self.next_states[self.position_buffer].copy_(next_states)
+            self.dones[self.position_buffer].copy_(dones.view(-1, 1))
+            self.position_buffer += 1
+        else:
+            raise BufferError
+        
+        # update pointers
+        if self.position_env >= self.num_envs:
+            self.position_env = 0
+            self.position_buffer += 1
+        if self.position_buffer >= self.buffer_size:
+            self.position_buffer = 0
             self.filled = True
 
-        self.states[self.position, 0].copy_(state)
-        self.actions[self.position, 0].copy_(action)
-        self.rewards[self.position, 0].copy_(reward.view(-1, 1))
-        self.next_states[self.position, 0].copy_(next_state)
-        self.dones[self.position, 0].copy_(done.view(-1, 1))
+    def sample(self, batch_size: int):
+        """
+        Sample a batch from the memory
 
-        self.position += 1
-
-    def add_transitions(self, states, actions, rewards, next_states, dones):
-        if self.position >= self.buffer_size:
-            self.position = 0
-
-        self.states[self.position].copy_(states)
-        self.actions[self.position].copy_(actions)
-        self.rewards[self.position].copy_(rewards.view(-1, 1))
-        self.next_states[self.position].copy_(next_states)
-        self.dones[self.position].copy_(dones.view(-1, 1))
-
-        self.position += 1
-
-    def push(self, state, action, reward, next_state, done):
-        self.add_transition(state, action, reward, next_state, done)
-
-    def sample(self, batch_size):
+        Parameters
+        ----------
+        batch_size: int
+            Number of element to sample
+        """
         raise NotImplementedError

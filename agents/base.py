ff5b360399e2f14396bd9ca81a518149eac4722a
Antonio Serrano Muñoz <aserrano@mondragon.edu>
2021-07-18 09:52:45 +0200

    Add docstrings to base class for agents

--- agents/base.py
+++ agents/base.py
@@ -1,6 +1,30 @@
+from typing import Union, Dict
+
+import gym
+import torch
+
+
+from ..env import Environment
+from ..memory import Memory
+from ..models.torch import Model
+
 
 class Agent:
-    def __init__(self, env, networks, memory=None, cfg: dict = {}) -> None:
+    def __init__(self, env: Union[Environment, gym.Env], networks: Dict[str, Model], memory: Union[Memory, None] = None, cfg: dict = {}) -> None:
+        """
+        Base class that represent a RL agent
+
+        Parameters
+        ----------
+        env: skrl.env.Environment or gym.Env
+            RL environment
+        networks: dictionary of skrl.models.torch.Model
+            Networks used by the agent
+        memory: skrl.memory.Memory or None
+            Memory to storage the transitions
+        cfg: dict
+            Configuration dictionary
+        """
         # TODO: get device from cfg
         self.device = "cuda:0"
 
@@ -9,19 +33,74 @@ class Agent:
         self.memory = memory
         self.cfg = cfg
 
-    def act(self, states, inference=False):
+    def act(self, states: torch.Tensor, inference: bool = False) -> torch.Tensor:
+        """
+        Process the environments' states to make a decision (actions) using the main policy
+
+        Parameters
+        ----------
+        states: torch.Tensor
+            Environments' states
+
+        Returns
+        -------
+        torch.Tensor
+            Actions
+        """
         raise NotImplementedError
 
-    def set_mode(self, mode):
+    def set_mode(self, mode: str) -> None:
+        """
+        Set the network mode (training or evaluation)
+
+        Parameters
+        ----------
+        mode: str
+            Mode: 'train' for training or 'eval' for evaluation
+        """
         for k in self.networks:
             self.networks[k].set_mode(mode)
 
-    def pre_rollout(self):
+    def pre_rollouts(self, timestep: int, timesteps: int) -> None:
+        """
+        Callback called before all rollouts
+
+        Parameters
+        ----------
+        timestep: int
+            Current timestep
+        timesteps: int
+            Number of timesteps
+        """
         pass
 
-    def inter_rollout(self, iteration, rollout):
+    def inter_rollouts(self, timestep: int, timesteps: int, rollout: int, rollouts: int) -> None:
+        """
+        Callback called after each rollout
+
+        Parameters
+        ----------
+        timestep: int
+            Current timestep
+        timesteps: int
+            Number of timesteps
+        rollout: int
+            Current rollout
+        rollouts: int
+            Number of rollouts
+        """
         pass
 
-    def post_rollout(self):
+    def post_rollouts(self, timestep: int, timesteps: int) -> None:
+        """
+        Callback called after all rollouts
+
+        Parameters
+        ----------
+        timestep: int
+            Current timestep
+        timesteps: int
+            Number of timesteps
+        """
         pass
     

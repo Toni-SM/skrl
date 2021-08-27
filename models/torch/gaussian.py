70f5dff958a292a809ee6b1e86b2d295820d4ed6
Antonio Serrano Muñoz <aserrano@mondragon.edu>
2021-08-27 16:58:55 +0200

    Clip gaussian policy actions and log standard deviations

--- models/torch/gaussian.py
+++ models/torch/gaussian.py
@@ -16,6 +16,10 @@ class GaussianModel(Model):
         super().__init__(env, device)
         
         self.parameters_log_std = None
+
+        self.clamp_log_std = True
+        self.log_std_min = -20.0
+        self.log_std_max = 2.0
         
     def act(self, states, taken_actions=None, inference=False):
         # map from states/observations to mean actions and log standard deviations
@@ -25,16 +29,26 @@ class GaussianModel(Model):
         if self.parameters_log_std is not None:
             log_std = self.parameters_log_std
         
+        # clamp log standard deviations
+        if self.clamp_log_std:
+            log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+
         # distribution
+        covariance = torch.diag(log_std.exp() * log_std.exp())
         if torch.numel(log_std) != self.num_action:
-            # FIXME: try to use all log_std
-            covariance = torch.diag(torch.mean(log_std.exp() * log_std.exp(), dim=0))
-        else:
-            covariance = torch.diag(log_std.exp() * log_std.exp())
+            covariance = covariance.unsqueeze(-1)
         distribution = MultivariateNormal(actions_mean, scale_tril=covariance)
 
-        # actions and log of the probability density function
-        actions = distribution.sample()
+        # actions # TODO: sample vs rsample
+        # actions = distribution.sample()
+        actions = distribution.rsample()
+
+        # clip actions # FIXME: use tensor too
+        actions.clamp_(self.env.action_space.low[0], self.env.action_space.high[0])
+        
+        # log of the probability density function
         log_prob = distribution.log_prob(actions)
+        if log_prob.dim() != actions.dim():
+            log_prob = log_prob.unsqueeze(-1)
 
         return actions, log_prob, actions_mean

e14abccfdd9d537d526bdb53dbcc6b1111130441
Antonio Serrano Muñoz <toni.semu@gmail.com>
2021-10-25 09:49:35 +0200

    Refactorize parameters to match base class

--- models/torch/deterministic.py
+++ models/torch/deterministic.py
@@ -1,16 +1,20 @@
+from __future__ import annotations
+from typing import Union, Tuple
+
+import gym
 import torch
 
 from . import Model
 
 
 class DeterministicModel(Model):
-    def __init__(self, env, device) -> None:
+    def __init__(self, observation_space: Union[int, tuple[int], gym.Space, None] = None, action_space: Union[int, tuple[int], gym.Space, None] = None, device: str = "cuda:0") -> None:
         """
         Deterministic model (Deterministic)
 
         # TODO: describe internal properties
         """
-        super().__init__(env, device)
+        super().__init__(observation_space=observation_space, action_space=action_space, device=device)
         
     def act(self, states, taken_actions=None, inference=False):
         # map from states/observations to actions

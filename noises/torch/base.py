cd8a8d887631fbe9100a199aed47f6c6d9fbc354
Antonio Serrano Muñoz <toni.semu@gmail.com>
2021-10-25 17:38:25 +0000

    Sample a noise with the same size as an input tensor

--- noises/torch/base.py
+++ noises/torch/base.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Tuple
 
 import torch
 
@@ -6,27 +6,46 @@ import torch
 class Noise():
     def __init__(self, device: str = "cuda:0") -> None:
         """
-        Base class that represent a noise
+        Base class representing a noise
 
         Parameters
         ----------
         device: str, optional
             Device on which a torch tensor is or will be allocated (default: "cuda:0")
         """
+        # TODO: what about parameters noise
         self.device = device
 
-    def sample(self, shape: Union[tuple[int], list[int], torch.Size]) -> torch.Tensor:
+    def sample_like(self, tensor: torch.Tensor) -> torch.Tensor:
         """
-        Sample noise
+        Sample a noise with the same size (shape) as the input tensor
+
+        This method will call the sampling method as follows `.sample(tensor.size())`
 
         Parameters
         ----------
-        shape: torch.Size or tuple or list of ints
-            Number of element on each dimension
+        tensor: torch.Tensor
+            Input tensor used to determine output tensor size (shape)
 
         Returns
         -------
-        tuple
+        torch.Tensor
+            Sampled noise
+        """
+        return self.sample(tensor.size())
+
+    def sample(self, size: Union[Tuple[int], torch.Size]) -> torch.Tensor:
+        """
+        Noise sampling method to be implemented by the inheriting classes
+
+        Parameters
+        ----------
+        size: tuple or list of ints or torch.Size
+            Shape of the sampled tensor
+
+        Returns
+        -------
+        torch.Tensor
             Sampled noise
         """
         raise NotImplementedError("The sampling method (.sample()) is not implemented")

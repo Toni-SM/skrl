12ff7ab79fa91e4ae8b9ee9de256d65d5d5646f7
Antonio Serrano Muñoz <toni.semu@gmail.com>
2021-11-01 09:14:31 +0000

    Add agent scopes

--- training.py
+++ training.py
@@ -14,11 +14,13 @@ class Trainer:
         RL trainer
         """
         self._current_learning_iteration = 0
-        self._multiagent = False
+        self._is_multi_agent = False
+
+        self._cfg = {}
+        self._env = None
 
         self._agents = None
-        self._env = None
-        self._cfg = {}
+        self._agents_scope = None
 
     def _pre_rollouts(self, timestep: int, timesteps: int) -> None:
         """
@@ -31,7 +33,7 @@ class Trainer:
         timesteps: int
             Number of timesteps
         """
-        if self._multiagent:
+        if self._is_multi_agent:
             for agent in self._agents:
                 agent.pre_rollouts(timestep=timestep, timesteps=timesteps)
         else:
@@ -52,7 +54,7 @@ class Trainer:
         rollouts: int
             Number of rollouts
         """
-        if self._multiagent:
+        if self._is_multi_agent:
             for agent in self._agents:
                 agent.inter_rollouts(timestep=timestep, timesteps=timesteps, rollout=rollout, rollouts=rollouts)
         else:
@@ -69,16 +71,16 @@ class Trainer:
         timesteps: int
             Number of timesteps
         """
-        if self._multiagent:
+        if self._is_multi_agent:
             for agent in self._agents:
                 agent.post_rollouts(timestep=timestep, timesteps=timesteps)
         else:
             self._agents.post_rollouts(timestep=timestep, timesteps=timesteps)
 
-    def train(self, agents: Union[Agent, List[Agent]], env: Union[Environment, gym.Env], cfg: dict = {}) -> None:
+    def train(self, cfg: dict, env: Union[Environment, gym.Env], agents: Union[Agent, List[Agent]], agents_scope : List[int] = []) -> None:
         """
-        Train the agents
-
+        # TODO: rewrite
+        Train the agent(s)
         Parameters
         ----------
         agents: skrl.agents.Agent or list of skrl.agents.Agent
@@ -89,20 +91,50 @@ class Trainer:
         cfg: dict
             Configuration dictionary
         """
-        # wrap env
-        self._env = wrap_env(env)
-
-        # verify multi-agent training
+        self._env = env
         self._agents = agents
-        if type(self._agents) is list:
-            if not hasattr(self._env, 'num_envs'):
-                raise AttributeError("The environment does not support parallelization (num_envs is not defined)")
-            if len(self._agents) != self._env.num_envs:
-                raise AssertionError("The number of agents ({}) does not match the number of parallelizable environments ({})".format(len(self._agents), self._env.num_envs))
-            self._multiagent = True
+        self._agents_scope = agents_scope
 
+        self._is_multi_agent = False
+
+        # wrap the environment to gym style
+        if issubclass(type(env), Environment):
+            self._env = wrap_env(env)
+        
+        if not hasattr(self._env, 'num_envs'):
+            raise AttributeError("The environment does not support parallelization (num_envs property is not defined)")
+
+        # validate agents and their scope
+        if type(agents) in [tuple, list]:
+            # single agent
+            if len(agents) == 1:
+                self._agents = agents[0]
+                self._is_multi_agent = False
+            # multi-agent
+            elif len(agents) > 1:
+                self._is_multi_agent = True
+                # check scope
+                if not len(agents_scope):
+                    print("[WARNING] The agents scope is empty")
+                    self._agents_scope = [int(self._env.num_envs / len(agents))] * len(agents)
+                    if sum(self._agents_scope):
+                        self._agents_scope[-1] += self._env.num_envs - sum(self._agents_scope)
+                    else:
+                        raise ValueError("The number of agents ({}) is greater than the number of parallelizable environments ({})".format(len(agents), self._env.num_envs))
+                elif len(agents_scope) != len(agents):
+                    raise ValueError("The number of agents ({}) doesn't match the number of scopes ({})".format(len(agents), len(agents_scope)))
+                elif sum(agents_scope) != self._env.num_envs:
+                    raise ValueError("The scopes ({}) don't cover the number of parallelizable environments ({})".format(sum(agents_scope), self._env.num_envs))
+
+                index = 0 
+                for i in range(len(self._agents_scope)):
+                    index += self._agents_scope[i]
+                    self._agents_scope[i] = (index - self._agents_scope[i], index)
+            else:
+                raise ValueError("The collection of agents is empty")
+        
         # enable train mode
-        if self._multiagent:
+        if self._is_multi_agent:
             for agent in self._agents:
                 agent.set_mode("train")
         else:
@@ -115,9 +147,20 @@ class Trainer:
         # get rollouts
         # for off-policy algorithms the notion of rollout corresponds to the steps taken in the environment between two updates (https://stable-baselines3.readthedocs.io/en/master/guide/callbacks.html?highlight=Rollout#custom-callback)
         rollouts = self._cfg.get("rollouts", 1)
-        if rollouts <= 0:
-            rollouts = 1
-        
+
+        # show information about RL system
+        print("")
+        print("RL system")
+        print("  |-- Number of parallelizable environments:", self._env.num_envs)
+        print("  |-- Multi-agent:", self._is_multi_agent)
+        print("  |-- Number of agents:", len(self._agents) if self._is_multi_agent else 1)
+        if self._is_multi_agent:
+            print("  |-- Agents and scopes:")
+            for agent, scope in zip(self._agents, self._agents_scope):
+                print("  |     |-- agent:", type(agent))
+                print("  |     |     |-- scope: {} environments ({}:{})".format(scope[1] - scope[0], scope[0], scope[1]))
+        print("")
+
         # reset env
         states = self._env.reset()
 
@@ -129,27 +172,28 @@ class Trainer:
             
             # rollouts
             for r in range(rollouts):
-                # compute the action
+                # compute actions
                 # TODO: sample controlled random actions
-                if self._multiagent:
-                    with torch.no_grad(): # TODO: verify use for single-agent
-                        actions = torch.stack([self._agents[i].act(states[i,:], inference=False) for i in range(len(self._agents))])
+                if self._is_multi_agent:
+                    actions = torch.stack([self._agents[i].act(states[i,:], inference=True) for i in range(len(self._agents))])
                 else:
-                    actions, _, _ = self._agents.act(states, inference=False)
-                    
+                    actions, _, _ = self._agents.act(states, inference=True)
+                
                 # step the environment
                 next_states, rewards, dones, infos = self._env.step(actions)
 
-                # record the transition
-                if self._multiagent:
-                    for i in range(len(self._agents)):
-                        if self._agents[i].memory is not None:
-                            self._agents[i].memory.add_transitions(states[i,:], actions[i,:], rewards[i,:], next_states[i,:], dones[i,:])
+                # record the transition 
+                if self._is_multi_agent:
+                    for agent, scope in zip(self._agents, self._agents_scope):
+                        agent.record_transition(states=states[scope[0]:scope[1]], 
+                                                actions=actions[scope[0]:scope[1]], 
+                                                rewards=rewards[scope[0]:scope[1]], 
+                                                next_states=next_states[scope[0]:scope[1]], 
+                                                dones=dones[scope[0]:scope[1]])
                 else:
-                    if self._agents.memory is not None:
-                        self._agents.memory.add_transitions(states, actions, rewards, next_states, dones)
+                    self._agents.record_transition(states, actions, rewards, next_states, dones)
                 
-                # reset env
+                # reset environments
                 # check boolean (Gym compatibility)
                 if isinstance(dones, bool):
                     states = self._env.reset() if dones else next_states

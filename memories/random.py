49892ec6979482b6838bb1f8eb236c7f5de0ee9c
Antonio Serrano Muñoz <toni.semu@gmail.com>
2021-11-09 12:55:34 +0100

    Sample random batch with or without replacement

--- memories/random.py
+++ memories/random.py
@@ -7,7 +7,7 @@ from .base import Memory
 
 
 class RandomMemory(Memory):
-    def __init__(self, memory_size: int, num_envs: int = 1, device: str = "cuda:0", preallocate: bool = True) -> None:
+    def __init__(self, memory_size: int, num_envs: int = 1, device: str = "cuda:0", preallocate: bool = True, replace=True) -> None:
         """
         Random sampling memory
 
@@ -23,8 +23,14 @@ class RandomMemory(Memory):
             Device on which a torch tensor is or will be allocated (default: "cuda:0")
         preallocate: bool, optional
             If true, preallocate memory for efficient use (default: True)
+        replace: bool, optional
+            Flag to indicate whether the sample is with or without replacement (default: True). 
+            Replacement implies that a value can be selected multiple times (the batch size is always guaranteed).
+            Sampling without replacement will return a batch of maximum memory size if the memory size is less than the requested batch size
         """
         super().__init__(memory_size, num_envs, device, preallocate)
+
+        self._replace = replace
 
     def sample(self, batch_size: int, names: Tuple[str]) -> Tuple[torch.Tensor]:
         """
@@ -44,6 +50,9 @@ class RandomMemory(Memory):
             The sampled tensors will have the following shape: (batch size, data size)
         """
         # generate random indexes
-        indexes = np.random.choice(len(self), size=batch_size, replace=True)
+        if self._replace:
+            indexes = np.random.choice(len(self), size=batch_size, replace=True)
+        else:
+            indexes = np.random.choice(len(self), size=min([batch_size, len(self)]), replace=False)
         
         return self.sample_by_index(indexes=indexes, names=names)

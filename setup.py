7ba77690272e3bf3e9e26eed51f83cd098804ee8
Antonio Serrano Muñoz <toni.semu@gmail.com>
2021-11-15 19:27:59 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import setuptools
+
+
+root_dir = os.path.dirname(os.path.realpath(__file__))
+
+# dependencies
+INSTALL_REQUIRES = [
+    "gym",
+    "torch",
+]
+
+# installation
+setuptools.setup(
+    name="skrl",
+    author="Toni-SM",
+    version="0.1.0",
+    description="Another Reinforcement Learning library :)",
+    long_description=open(os.path.join(root_dir, "README.md")).read(),
+    long_description_content_type="text/markdown",
+    keywords=["rl"],
+    include_package_data=True,
+    python_requires=">=3.6.*",
+    install_requires=INSTALL_REQUIRES,
+    url="https://github.com/Toni-SM/skrl",
+    packages=setuptools.find_packages("."),
+    classifiers=[
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    zip_safe=False,
+)

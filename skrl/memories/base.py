Fix return and adventage functions computation

skrl/memories/base.py
@@ -1,4 +1,4 @@
-from typing import Union, Tuple
+from typing import Dict, Union, Tuple
 
 import gym
 import torch
@@ -236,16 +236,13 @@ class Memory:
         tensors = [getattr(self, "_tensor_{}".format(name)) for name in names]
         return [tensor.view(-1, tensor.size(-1)) for tensor in tensors]
 
-    def compute_functions(self, states_src: str = "states", actions_src: str = "actions", rewards_src: str = "rewards", next_states_src: str = "next_states", dones_src: str = "dones", values_src: str = "values", returns_dst: Union[str, None] = None, advantages_dst: Union[str, None] = None) -> None:
+    def compute_functions(self, states_src: str = "states", actions_src: str = "actions", rewards_src: str = "rewards", next_states_src: str = "next_states", dones_src: str = "dones", values_src: str = "values", returns_dst: Union[str, None] = None, advantages_dst: Union[str, None] = None, last_values: Union[torch.Tensor, None] = None, hyperparameters: Dict = {"discount_factor": 0.99, "lambda_parameter": 0.95, "normalize_returns": False, "normalize_advantages": True}) -> None:
         """
         Compute the following functions for the given tensor names
 
-        # TODO: add documentation about the functions
-        Functions:
-        - returns:
-            (returns - values) + (advantages * gamma)
-        - advantages:
-            (returns - values)
+        Available functions:
+        - Returns (total discounted reward)
+        - Advantages (total discounted reward - baseline)
 
         Parameters
         ----------
@@ -265,42 +262,52 @@ class Memory:
             Name of the tensor where the returns will be stored (default: None)
         advantages_dst: str or None, optional
             Name of the tensor where the advantages will be stored (default: None)
+        last_values: torch.Tensor or None, optional
+            Last values (default: None).
+            If None, the last values will be obtained from the tensor containing the values
+        hyperparameters: dict, optional
+            Hyperparameters to control the computation of the functions
+            The following hyperparameters are expected:
+            - discount_factor: float
+                Discount factor (gamma) for the computation of the returns and the advantages (default: 0.99)
+            - lambda_parameter: float
+                Lambda parameter (lam) for the computation of the returns and the advantages (default: 0.95)
+            - normalize_returns: bool
+                If True, the returns will be normalized (default: False)
+            - normalize_advantages: bool
+                If True, the advantages will be normalized (default: True)
         """
-        # get source tensors
-        # states = getattr(self, "_tensor_{}".format(states_src))
-        # actions = getattr(self, "_tensor_{}".format(actions_src))
+        # TODO: compute functions attending the circular buffer logic
+        # TODO: get last values from the last samples (if not provided) and ignore them in the computation
+
+        # get source and destination tensors
         rewards = getattr(self, "_tensor_{}".format(rewards_src))
-        # next_states = getattr(self, "_tensor_{}".format(next_states_src))
         dones = getattr(self, "_tensor_{}".format(dones_src))
         values = getattr(self, "_tensor_{}".format(values_src))
         
         returns = getattr(self, "_tensor_{}".format(returns_dst)) if returns_dst is not None else torch.zeros_like(rewards)
         advantages = getattr(self, "_tensor_{}".format(advantages_dst)) if advantages_dst is not None else torch.zeros_like(rewards)
 
-        # TODO: get variables from the config
-        gamma = 0.99
-        lam = 0.95
+        # hyperarameters
+        discount_factor = hyperparameters.get("discount_factor", 0.99)
+        lambda_parameter = hyperparameters.get("lambda_parameter", 0.95)
+        normalize_returns = hyperparameters.get("normalize_returns", False)
+        normalize_advantages = hyperparameters.get("normalize_advantages", True)
 
-        advantage = 0
-        last_values = values[-1]
-
-        # compute the returns
+        # compute and normalize the returns
         if returns_dst is not None or advantages_dst is not None:
+            advantage = 0
             for step in reversed(range(self.memory_size)):
-                if step == self.memory_size - 1:
-                    next_values = last_values
-                else:
-                    next_values = values[step + 1]
-
-                # not_dones = dones[step].logical_not()
-                # delta = rewards[step] + not_dones * gamma * next_values - values[step]
-                # advantage = delta + not_dones * gamma * lam * advantage
-                advantage = rewards[step] - values[step] + gamma * dones[step].logical_not() * (next_values + lam * advantage)
-                
+                next_values = values[step + 1] if step < self.memory_size - 1 else last_values
+                advantage = rewards[step] - values[step] + discount_factor * dones[step].logical_not() * (next_values + lambda_parameter * advantage)
                 returns[step].copy_(advantage + values[step])
+
+            if normalize_returns:
+                returns.copy_((returns - returns.mean()) / (returns.std() + 1e-8))
 
         # compute and normalize the advantages
         if advantages_dst is not None:
             advantages.copy_(returns - values)
-            advantages.copy_((advantages - advantages.mean()) / (advantages.std() + 1e-8))
+            if normalize_advantages:
+                advantages.copy_((advantages - advantages.mean()) / (advantages.std() + 1e-8))
         
d25fb6408c2cf1d62c9bb08b11c1721b6aafc105
Antonio Serrano Muñoz <toni.semu@gmail.com>
2021-11-23 12:54:43 +0000

    Format docstrings for sphinx

--- skrl/models/torch/base.py
+++ skrl/models/torch/base.py
@@ -7,21 +7,18 @@ import numpy as np
 
 class Model(torch.nn.Module):
     def __init__(self, observation_space: Union[int, Tuple[int], gym.Space, None] = None, action_space: Union[int, Tuple[int], gym.Space, None] = None, device: str = "cuda:0") -> None:
-        """
-        Base class representing a neural network model
+        """Base class representing a function approximator
 
         # TODO: describe internal properties and methods
 
-        Parameters
-        ----------
-        observation_space: int, tuple or list of integers, gym.Space or None, optional
-            Observation/state space or shape (default: None).
-            If it is not None, the num_observations property will contain the size of that space (number of elements)
-        action_space: int, tuple or list of integers, gym.Space or None, optional
-            Action space or shape (default: None).
-            If it is not None, the num_actions property will contain the size of that space (number of elements)
-        device: str, optional
-            Device on which a torch tensor is or will be allocated (default: "cuda:0")
+        :param observation_space: Observation/state space or shape (default: None).
+                                  If it is not None, the num_observations property will contain the size of that space (number of elements)
+        :type observation_space: int, tuple or list of integers, gym.Space or None, optional
+        :param action_space: Action space or shape (default: None).
+                             If it is not None, the num_actions property will contain the size of that space (number of elements)
+        :type action_space: int, tuple or list of integers, gym.Space or None, optional
+        :param device: Device on which a torch tensor is or will be allocated (default: "cuda:0")
+        :type device: str, optional
         """
         super(Model, self).__init__()
 
@@ -33,18 +30,13 @@ class Model(torch.nn.Module):
         self.num_actions = None if action_space is None else self._get_space_size(action_space)
         
     def _get_space_size(self, space: Union[int, Tuple[int], gym.Space]) -> int:
-        """
-        Get the size (number of elements) of a space
+        """Get the size (number of elements) of a space
 
-        Parameters
-        ----------
-        space: int, tuple or list of integers, gym.Space or None
-           Space or shape from which to obtain the number of elements
-        
-        Returns
-        -------
-        int
-            Space size (number of elements)
+        :param space: Space or shape from which to obtain the number of elements
+        :type space: int, tuple or list of integers, or gym.Space
+
+        :return: [description]
+        :rtype: Space size (number of elements)
         """
         if type(space) in [tuple, list]:
             return np.prod(space)
@@ -55,18 +47,13 @@ class Model(torch.nn.Module):
         return space
 
     def random_act(self, states: torch.Tensor) -> Tuple[torch.Tensor]:
-        """
-        Act randomly
+        """Act randomly
 
-        Parameters
-        ----------
-        states: torch.Tensor
-            Observation/state of the environment used to get the shape of the action space
-            
-        Returns
-        -------
-        tuple of torch.Tensor
-            Random action to be taken by the agent
+        :param states: Observation/state of the environment used to get the shape of the action space
+        :type states: torch.Tensor
+
+        :return: Random actions to be taken by the agent
+        :rtype: tuple of torch.Tensor
         """
         # TODO: sample taking into account states' shape
         # TODO: sample taking into account bounds
@@ -75,42 +62,36 @@ class Model(torch.nn.Module):
         return actions.to(self.device), None, None
 
     def init_parameters(self, method_name: str = "normal_", *args, **kwargs) -> None:
-        """
-        Initialize the model parameters according to the specified method name
+        """Initialize the model parameters according to the specified method name
 
         Method names are from the [torch.nn.init](https://pytorch.org/docs/stable/nn.init.html) module. 
-        Allowed method names are "uniform_", "normal_", "constant_", etc.
+        Allowed method names are `uniform_`, `normal_`, `constant_`, etc.
 
-        Parameters
-        ----------
-        method_name: str, optional
-            Name of the [torch.nn.init](https://pytorch.org/docs/stable/nn.init.html) method (default: "normal_")
-        args: tuple, optional
-            Positional arguments of the method to be called
-        kwargs: dict, optional
-            Key-value arguments of the method to be called
+        :param method_name: Name of the [torch.nn.init](https://pytorch.org/docs/stable/nn.init.html) method (default: ``"normal_"``)
+        :type method_name: str, optional
+        :param args: Positional arguments of the method to be called
+        :type args: tuple, optional
+        :param kwargs: Key-value arguments of the method to be called
+        :type kwargs: dict, optional
         """
         for parameters in self.parameters():
             exec("torch.nn.init.{}(parameters, *args, **kwargs)".format(method_name))
 
     def init_weights(self, method_name: str = "orthogonal_", *args, **kwargs) -> None:
-        """
-        Initialize the model weights according to the specified method name
-
+        """Initialize the model weights according to the specified method name
+        
         Method names are from the [torch.nn.init](https://pytorch.org/docs/stable/nn.init.html) module. 
-        Allowed method names are "uniform_", "normal_", "constant_", etc.
+        Allowed method names are `uniform_`, `normal_`, `constant_`, etc.
 
         The following layers will be initialized:
         - torch.nn.Linear
-
-        Parameters
-        ----------
-        method_name: str, optional
-            Name of the [torch.nn.init](https://pytorch.org/docs/stable/nn.init.html) method (default: "orthogonal_")
-        args: tuple, optional
-            Positional arguments of the method to be called
-        kwargs: dict, optional
-            Key-value arguments of the method to be called
+        
+        :param method_name: Name of the [torch.nn.init](https://pytorch.org/docs/stable/nn.init.html) method (default: ``"orthogonal_"``)
+        :type method_name: str, optional
+        :param args: Positional arguments of the method to be called
+        :type args: tuple, optional
+        :param kwargs: Key-value arguments of the method to be called
+        :type kwargs: dict, optional
         """
         def _update_weights(module, method_name, args, kwargs):
             for layer in module:
@@ -122,63 +103,60 @@ class Model(torch.nn.Module):
         _update_weights(self.children(), method_name, args, kwargs)
 
     def forward(self):
+        """Forward pass of the model
+
+        :raises NotImplementedError: Child class must ``.act()`` and ``.compute()`` methods
+        """
         raise NotImplementedError("Implement .act() and .compute() methods instead of this")
 
     def compute(self, states: torch.Tensor, taken_actions: Union[torch.Tensor, None] = None) -> Union[torch.Tensor, Tuple[torch.Tensor]]:
-        """
-        Define the computation performed (to be implemented by the inheriting classes) by all involved networks
+        """Define the computation performed (to be implemented by the inheriting classes) by all involved networks
 
-        Parameters
-        ----------
-        states: torch.Tensor
-            Observation/state of the environment used to make the decision
-        taken_actions: torch.Tensor or None, optional
-            Actions taken by a policy to the given states (default: None).
-            The use of these actions only makes sense in critical networks, e.g.
+        :param states: Observation/state of the environment used to make the decision
+        :type states: torch.Tensor
+        :param taken_actions: Actions taken by a policy to the given states (default: None).
+                              The use of these actions only makes sense in critical networks, e.g.
+        :type taken_actions: torch.Tensor or None, optional
 
-        Returns
-        -------
-        torch.Tensor or tuple of torch.Tensor
-            Computation performed by all involved networks
+        :raises NotImplementedError: Child class must implement this method
+        
+        :return: Computation performed by all involved networks
+        :rtype: torch.Tensor or tuple of torch.Tensor
         """
         raise NotImplementedError("The computation performed by all involved networks (.compute()) is not implemented")
 
     def act(self, states: torch.Tensor, taken_actions: Union[torch.Tensor, None] = None, inference=False) -> Tuple[torch.Tensor]:
-        """
-        Act according to the specified behavior (to be implemented by the inheriting classes)
+        """Act according to the specified behavior (to be implemented by the inheriting classes)
 
         Agents will call this method, during training and evaluation, to obtain the decision to be taken given the state of the environment.
         This method is currently implemented in the predefined models (GaussianModel, DeterministicModel, etc.).
         The classes that inherit from the latter must only implement the .compute() method
+
+        :param states: Observation/state of the environment used to make the decision
+        :type states: torch.Tensor
+        :param taken_actions: Actions taken by a policy to the given states (default: None).
+                              The use of these actions only makes sense in critical networks, e.g.
+        :type taken_actions: torch.Tensor or None, optional
+        :param inference: Flag to indicate whether the network is making inference (default: False)
+        :type inference: bool, optional
+
+        :raises NotImplementedError: Child class must implement this method
         
-        Parameters
-        ----------
-        states: torch.Tensor
-            Observation/state of the environment used to make the decision
-        taken_actions: torch.Tensor or None, optional
-            Actions taken by a policy to the given states (default: None).
-            The use of these actions only makes sense in critical networks, e.g.
-        inference: bool, optional
-            Flag to indicate whether the network is making inference (default: False)
-        
-        Returns
-        -------
-        tuple of torch.Tensor
-            Action to be taken by the agent given the state of the environment.
-            The typical tuple's components are the actions, the log of the probability density function and mean actions.
-            Deterministic agents must ignore the last two components and return empty tensors or None for them
+        :return: Action to be taken by the agent given the state of the environment.
+                 The typical tuple's components are the actions, the log of the probability density function and mean actions.
+                 Deterministic agents must ignore the last two components and return empty tensors or None for them
+        :rtype: tuple of torch.Tensor
         """
         raise NotImplementedError("The action to be taken by the agent (.act()) is not implemented")
         
     def set_mode(self, mode: str) -> None:
-        """
-        Set the network mode (training or evaluation)
+        """Set the network mode (training or evaluation)
 
-        Parameters
-        ----------
-        mode: str
-            Mode: "train" for training or "eval" for evaluation.
-            https://pytorch.org/docs/stable/generated/torch.nn.Module.html?highlight=torch%20nn%20module%20train#torch.nn.Module.train
+        :param mode: Mode: "train" for training or "eval" for evaluation.
+                     https://pytorch.org/docs/stable/generated/torch.nn.Module.html?highlight=torch%20nn%20module%20train#torch.nn.Module.train
+        :type mode: str
+
+        :raises ValueError: Mode must be "train" or "eval"
         """
         if mode == "train":
             self.train(True)
@@ -198,34 +176,28 @@ class Model(torch.nn.Module):
         pass
     
     def freeze_parameters(self, freeze: bool = True) -> None:
-        """
-        Freeze or unfreeze internal parameters
+        """Freeze or unfreeze internal parameters
 
         - Freeze: disable gradient computation (`parameters.requires_grad = False`)
         - Unfreeze: enable gradient computation (`parameters.requires_grad = True`) 
         
-        Parameters
-        ----------
-        freeze: bool
-            Freeze the internal parameters if True, otherwise unfreeze them
+        :param freeze: Freeze the internal parameters if True, otherwise unfreeze them
+        :type freeze: bool, optional
         """
         for parameters in self.parameters():
             parameters.requires_grad = not freeze
 
     def update_parameters(self, network: torch.nn.Module, polyak: float = 0) -> None:
-        """
-        Update internal parameters by hard or soft (polyak averaging) update
+        """Update internal parameters by hard or soft (polyak averaging) update
 
         - Hard update: `parameters = network.parameters`
         - Soft (polyak averaging) update: `parameters = polyak * parameters + (1 - polyak) * network.parameters`
 
-        Parameters
-        ----------
-        network: torch.nn.Module (skrl.models.torch.Model)
-            Network used to update the internal parameters
-        polyak: float, optional
-            Polyak hyperparameter between 0 and 1 (usually close to 1).
-            A hard update is performed when its value is 0 (default)
+        :param network: Network used to update the internal parameters
+        :type network: torch.nn.Module (skrl.models.torch.Model)
+        :param polyak: Polyak hyperparameter between 0 and 1 (usually close to 1).
+                       A hard update is performed when its value is 0 (default)
+        :type polyak: float, optional
         """
         with torch.no_grad():
             # hard update

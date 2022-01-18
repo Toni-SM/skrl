7bbbc24c37871a822ddd602849ae194b0ebab6f9
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-01-18 20:33:22 +0000

    Move trainer logic to SequentialTrainer

--- skrl/trainers/torch/base.py
+++ skrl/trainers/torch/base.py
@@ -2,14 +2,16 @@ from typing import Union, List
 
 import time
 
-import torch
-
 from ...envs.torch import Wrapper
 from ...agents.torch import Agent
 
 
 class Trainer():
-    def __init__(self, cfg: dict, env: Wrapper, agents: Union[Agent, List[Agent], List[List[Agent]]], agents_scope : List[int] = []) -> None:
+    def __init__(self, 
+                 cfg: dict, 
+                 env: Wrapper, 
+                 agents: Union[Agent, List[Agent], List[List[Agent]]], 
+                 agents_scope : List[int] = []) -> None:
         """Base class for trainers
 
         :param cfg: Configuration dictionary
@@ -21,21 +23,23 @@ class Trainer():
         :param agents_scope: Number of environments for each agent to train on (default: [])
         :type agents_scope: tuple or list of integers
         """
-        self.starting_timestep = 0
-        
+        self.cfg = cfg
         self.env = env
-
-        self.num_agents = 0
         self.agents = agents
         self.agents_scope = agents_scope
-        self.parallel_agents = False
+        
+        # get configuration
+        self.timesteps = self.cfg.get('timesteps', 0)
+        self.headless = self.cfg.get("headless", False)
+        self.progress_interval = self.cfg.get("progress_interval", 1000)
 
-        self.timesteps = cfg.get('timesteps', 0)
-        self.headless = cfg.get("headless", False)
+        self.initial_timestep = 0
 
         self._timestamp = None
         self._timestamp_elapsed = None
 
+        # setup agents
+        self.num_agents = 0
         self._setup_agents()
 
     def __str__(self) -> str:
@@ -46,143 +50,107 @@ class Trainer():
         """
         string = "Trainer: {}".format(repr(self))
         string += "\n  |-- Number of parallelizable environments: {}".format(self.env.num_envs)
-        string += "\n  |-- Parallel agents: {}".format(self.parallel_agents)
-        string += "\n  |-- Number of agents: {}".format(len(self.agents) if self.parallel_agents else 1)
-        if self.parallel_agents:
-            string += "\n  |-- Agents and scopes:"
+        string += "\n  |-- Number of agents: {}".format(self.num_agents)
+        string += "\n  |-- Agents and scopes:"
+        if self.num_agents > 1:
             for agent, scope in zip(self.agents, self.agents_scope):
                 string += "\n  |     |-- agent: {}".format(type(agent))
                 string += "\n  |     |     |-- scope: {} environments ({}:{})".format(scope[1] - scope[0], scope[0], scope[1])
+        else:
+            string += "\n  |     |-- agent: {}".format(type(self.agents))
+            string += "\n  |     |     |-- scope: {} environment(s)".format(self.env.num_envs)
         return string
-
-    def _pre_interaction(self, timestep: int, timesteps: int) -> None:
-        """Call pre_interaction method for each agent before all interactions
-
-        :param timestep: Current timestep
-        :type timestep: int
-        :param timesteps: Number of timesteps
-        :type timesteps: int
-        """
-        pass
-
-    def _post_interaction(self, timestep: int, timesteps: int) -> None:
-        """Call post_interaction method for each agent after all interactions
-        
-        :param timestep: Current timestep
-        :type timestep: int
-        :param timesteps: Number of timesteps
-        :type timesteps: int
-        """
-        pass
 
     def _setup_agents(self) -> None:
         """Setup agents for training
 
         :raises ValueError: Invalid setup
         """
-        # validate agents and their scope
+        # validate agents and their scopes
         if type(self.agents) in [tuple, list]:
             # single agent
             if len(self.agents) == 1:
+                self.num_agents = 1
                 self.agents = self.agents[0]
-                self.parallel_agents = False
+                self.agents_scope = [1]
             # parallel agents
             elif len(self.agents) > 1:
-                self.parallel_agents = True
-                # check scope
+                self.num_agents = len(self.agents)
+                # check scopes
                 if not len(self.agents_scope):
-                    print("[WARNING] The agents scope is empty")
+                    print("[WARNING] The agents' scopes are empty, they will be generated as equal as possible")
                     self.agents_scope = [int(self.env.num_envs / len(self.agents))] * len(self.agents)
                     if sum(self.agents_scope):
                         self.agents_scope[-1] += self.env.num_envs - sum(self.agents_scope)
                     else:
-                        raise ValueError("The number of agents ({}) is greater than the number of parallelizable environments ({})".format(len(self.agents), self.env.num_envs))
+                        raise ValueError("The number of agents ({}) is greater than the number of parallelizable environments ({})". \
+                            format(len(self.agents), self.env.num_envs))
                 elif len(self.agents_scope) != len(self.agents):
-                    raise ValueError("The number of agents ({}) doesn't match the number of scopes ({})".format(len(self.agents), len(self.agents_scope)))
+                    raise ValueError("The number of agents ({}) doesn't match the number of scopes ({})". \
+                        format(len(self.agents), len(self.agents_scope)))
                 elif sum(self.agents_scope) != self.env.num_envs:
-                    raise ValueError("The scopes ({}) don't cover the number of parallelizable environments ({})".format(sum(self.agents_scope), self.env.num_envs))
-
+                    raise ValueError("The scopes ({}) don't cover the number of parallelizable environments ({})". \
+                        format(sum(self.agents_scope), self.env.num_envs))
+                # generate agents' scopes
                 index = 0 
                 for i in range(len(self.agents_scope)):
                     index += self.agents_scope[i]
                     self.agents_scope[i] = (index - self.agents_scope[i], index)
             else:
-                raise ValueError("There are no agents to train")
-        
-        self.num_agents = len(self.agents) if self.parallel_agents else 1
+                raise ValueError("A list of agents is expected")
         
         # enable train mode
-        if self.parallel_agents:
+        if self.num_agents > 1:
             for agent in self.agents:
                 agent.set_mode("train")
         else:
             self.agents.set_mode("train")
         
+    def show_progress(self, timestep: int, timesteps: int) -> None:
+        """Show training progress
+
+        :param timestep: Current timestep
+        :type timestep: int
+        :param timesteps: Number of timesteps
+        :type timesteps: int
+        """
+        if timestep > 0:
+            timestep += 1
+        
+        if not timestep % self.progress_interval:
+            current_timestamp = time.time()
+            if self._timestamp is None:
+                self._timestamp = current_timestamp
+                self._timestamp_elapsed = self._timestamp
+
+            delta = current_timestamp - self._timestamp
+            elapsed = current_timestamp - self._timestamp_elapsed if timestep else 0.0
+            remaining = elapsed * (self.timesteps / timestep - 1) if timestep else 0.0
+            
+            self._timestamp = current_timestamp
+
+            print("|--------------------------|--------------------------|")
+            print("|     timestep / timesteps | {} / {}".format(timestep, self.timesteps))
+            print("|     timesteps per second |", round(self.progress_interval / delta, 2) if timestep else 0.0)
+            print("| elapsed / remaining time | {} sec / {} sec".format(round(elapsed, 2), round(remaining, 2)))
+
+    def train(self) -> None:
+        """Train the agents
+
+        :raises NotImplementedError: Not implemented
+        """
+        raise NotImplementedError
+
+    def eval(self) -> None:
+        """Evaluate the agents
+
+        :raises NotImplementedError: Not implemented
+        """
+        raise NotImplementedError
+
     def start(self) -> None:
         """Start training
+
+        This method is deprecated in favour of the '.train()' method 
         """
-        # reset env
-        states = self.env.reset()
-
-        for timestep in range(self.starting_timestep, self.timesteps):
-            # print progress
-            if timestep % 1000 == 0:
-                if self._timestamp is None:
-                    self._timestamp = time.time()
-                    self._timestamp_elapsed = self._timestamp
-                delta = time.time() - self._timestamp
-                self._timestamp = time.time()
-
-                print("------------------------------------------------------")
-                print("| total timesteps:", timestep)
-                print("| timesteps per seconds:", round(1000 / delta, 2) if timestep else 0.0)
-                print("| elapsed time (seconds):", round(time.time() - self._timestamp_elapsed, 2) if timestep else 0.0)
-            
-            # pre-interaction
-            self._pre_interaction(timestep=timestep, timesteps=self.timesteps)
-            
-            # compute actions
-            with torch.no_grad():
-                if self.parallel_agents:
-                    actions = torch.vstack([agent.act(states[scope[0]:scope[1]], 
-                                                    inference=True,
-                                                    timestep=timestep, 
-                                                    timesteps=self.timesteps)[0] for agent, scope in zip(self.agents, self.agents_scope)])
-                else:
-                    actions, _, _ = self.agents.act(states, inference=True, timestep=timestep, timesteps=self.timesteps)
-            
-            # step the environment
-            next_states, rewards, dones, infos = self.env.step(actions)
-            
-            # render scene
-            if not self.headless:
-                self.env.render()
-
-            # record the transition 
-            with torch.no_grad():
-                if self.parallel_agents:
-                    for agent, scope in zip(self.agents, self.agents_scope):
-                        agent.record_transition(states=states[scope[0]:scope[1]], 
-                                                actions=actions[scope[0]:scope[1]], 
-                                                rewards=rewards[scope[0]:scope[1]], 
-                                                next_states=next_states[scope[0]:scope[1]], 
-                                                dones=dones[scope[0]:scope[1]],
-                                                timestep=timestep,
-                                                timesteps=self.timesteps)
-                else:
-                    self.agents.record_transition(states=states, 
-                                                actions=actions,
-                                                rewards=rewards,
-                                                next_states=next_states,
-                                                dones=dones,
-                                                timestep=timestep,
-                                                timesteps=self.timesteps)
-            
-            # reset environments
-            if dones.any():
-                states = self.env.reset()
-            else:
-                states.copy_(next_states)
-                
-            # post-interaction
-            self._post_interaction(timestep=timestep, timesteps=self.timesteps)
+        print("[WARNING] Trainer.start() method is deprecated in favour of the '.train()' method")
--- skrl/trainers/torch/sequential.py
+++ skrl/trainers/torch/sequential.py
@@ -1,5 +1,7 @@
 from typing import Union, List
 
+import torch
+
 from ...envs.torch import Wrapper
 from ...agents.torch import Agent
 
@@ -7,10 +9,14 @@ from . import Trainer
 
 
 class SequentialTrainer(Trainer):
-    def __init__(self, cfg: dict, env: Wrapper, agents: Union[Agent, List[Agent], List[List[Agent]]], agents_scope : List[int] = []) -> None:
+    def __init__(self, 
+                 cfg: dict, 
+                 env: Wrapper, 
+                 agents: Union[Agent, List[Agent], List[List[Agent]]], 
+                 agents_scope : List[int] = []) -> None:
         """Sequential trainer
         
-        Train parallel agents sequentially, i.e. one after another.
+        Train agents sequentially (i.e., one after the other in each interaction with the environment)
 
         :param cfg: Configuration dictionary
         :type cfg: dict
@@ -23,30 +29,129 @@ class SequentialTrainer(Trainer):
         """
         super().__init__(cfg, env, agents, agents_scope)
 
-    def _pre_interaction(self, timestep: int, timesteps: int) -> None:
-        """Call pre_interaction method for each agent before all interactions
+    def train(self) -> None:
+        """Train the agents sequentially
 
-        :param timestep: Current timestep
-        :type timestep: int
-        :param timesteps: Number of timesteps
-        :type timesteps: int
+        Execute the following steps in loop:
+        - Pre-interaction (sequentially)
+        - Compute actions (sequentially)
+        - Interact with the environments
+        - Render scene
+        - Record transitions (sequentially)
+        - Reset environments
+        - Post-interaction (sequentially)
         """
-        if self.parallel_agents:
-            for agent in self.agents:
-                agent.pre_interaction(timestep=timestep, timesteps=timesteps)
-        else:
-            self.agents.pre_interaction(timestep=timestep, timesteps=timesteps)
+        # reset env
+        states = self.env.reset()
 
-    def _post_interaction(self, timestep: int, timesteps: int) -> None:
-        """Call post_interaction method for each agent after all interactions
+        for timestep in range(self.initial_timestep, self.timesteps):
+            # show progress
+            self.show_progress(timestep=timestep, timesteps=self.timesteps)
+
+            # pre-interaction
+            if self.num_agents > 1:
+                for agent in self.agents:
+                    agent.pre_interaction(timestep=timestep, timesteps=self.timesteps)
+            else:
+                self.agents.pre_interaction(timestep=timestep, timesteps=self.timesteps)
+            
+            # compute actions
+            with torch.no_grad():
+                if self.num_agents > 1:
+                    actions = torch.vstack([agent.act(states[scope[0]:scope[1]], 
+                                                      inference=True,
+                                                      timestep=timestep, 
+                                                      timesteps=self.timesteps)[0] \
+                                            for agent, scope in zip(self.agents, self.agents_scope)])
+                else:
+                    actions, _, _ = self.agents.act(states, inference=True, timestep=timestep, timesteps=self.timesteps)
+            
+            # step the environments
+            next_states, rewards, dones, infos = self.env.step(actions)
+            
+            # render scene
+            if not self.headless:
+                self.env.render()
+
+            # record the environments' transitions
+            with torch.no_grad():
+                if self.num_agents > 1:
+                    for agent, scope in zip(self.agents, self.agents_scope):
+                        agent.record_transition(states=states[scope[0]:scope[1]], 
+                                                actions=actions[scope[0]:scope[1]], 
+                                                rewards=rewards[scope[0]:scope[1]], 
+                                                next_states=next_states[scope[0]:scope[1]], 
+                                                dones=dones[scope[0]:scope[1]],
+                                                timestep=timestep,
+                                                timesteps=self.timesteps)
+                else:
+                    self.agents.record_transition(states=states, 
+                                                  actions=actions,
+                                                  rewards=rewards,
+                                                  next_states=next_states,
+                                                  dones=dones,
+                                                  timestep=timestep,
+                                                  timesteps=self.timesteps)
+            
+            # reset environments
+            with torch.no_grad():
+                if dones.any():
+                    states = self.env.reset()
+                else:
+                    states.copy_(next_states)
+                
+            # post-interaction
+            if self.num_agents > 1:
+                for agent in self.agents:
+                    agent.post_interaction(timestep=timestep, timesteps=self.timesteps)
+            else:
+                self.agents.post_interaction(timestep=timestep, timesteps=self.timesteps)
         
-        :param timestep: Current timestep
-        :type timestep: int
-        :param timesteps: Number of timesteps
-        :type timesteps: int
+    def eval(self) -> None:
+        """Evaluate the agents sequentially
+
+        Execute the following steps in loop:
+        - Compute actions (sequentially)
+        - Interact with the environments
+        - Render scene
+        - Reset environments
         """
-        if self.parallel_agents:
-            for agent in self.agents:
-                agent.post_interaction(timestep=timestep, timesteps=timesteps)
-        else:
-            self.agents.post_interaction(timestep=timestep, timesteps=timesteps)
+        # reset env
+        states = self.env.reset()
+
+        for timestep in range(self.initial_timestep, self.timesteps):
+            # show progress
+            self.show_progress(timestep=timestep, timesteps=self.timesteps)
+            
+            # compute actions
+            with torch.no_grad():
+                if self.num_agents > 1:
+                    actions = torch.vstack([agent.act(states[scope[0]:scope[1]], 
+                                                      inference=True,
+                                                      timestep=timestep, 
+                                                      timesteps=self.timesteps)[0] \
+                                            for agent, scope in zip(self.agents, self.agents_scope)])
+                else:
+                    actions, _, _ = self.agents.act(states, inference=True, timestep=timestep, timesteps=self.timesteps)
+            
+            # step the environments
+            next_states, rewards, dones, infos = self.env.step(actions)
+            
+            # render scene
+            if not self.headless:
+                self.env.render()
+            
+            # reset environments
+            with torch.no_grad():
+                if dones.any():
+                    states = self.env.reset()
+                else:
+                    states.copy_(next_states)
+        
+    def start(self) -> None:
+        """Start training
+
+        This method is deprecated in favour of the '.train()' method 
+        """
+        super().start()
+        self.train()

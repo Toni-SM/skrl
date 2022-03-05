094b8117f583053274cf63c61b7f9422eea2ff45
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-03-05 15:03:26 +0000

    Add SARSA agent

---
+++ skrl/agents/torch/sarsa/sarsa.py
@@ -0,0 +1,214 @@
+from typing import Union, Tuple, Dict
+
+import gym
+import copy
+
+import torch
+
+from ....memories.torch import Memory
+from ....models.torch import Model
+
+from .. import Agent
+
+
+SARSA_DEFAULT_CONFIG = {
+    "discount_factor": 0.99,        # discount factor (gamma)
+
+    "random_timesteps": 1000,       # random exploration steps
+    "learning_starts": 1000,        # learning starts after this many steps
+
+    "epsilon": 0.1,                 # epsilon-greedy exploration
+    "alpha": 0.5,                   # learning rate
+
+    "experiment": {
+        "directory": "",            # experiment's parent directory
+        "experiment_name": "",      # experiment name
+        "write_interval": 250,      # TensorBoard writing interval (timesteps)
+
+        "checkpoint_interval": 1000,        # interval for checkpoints (timesteps)
+        "checkpoint_policy_only": True,     # checkpoint for policy only
+    }
+}
+
+
+class SARSA(Agent):
+    def __init__(self, 
+                 models: Dict[str, Model], 
+                 memory: Union[Memory, Tuple[Memory], None] = None, 
+                 observation_space: Union[int, Tuple[int], gym.Space, None] = None, 
+                 action_space: Union[int, Tuple[int], gym.Space, None] = None, 
+                 device: Union[str, torch.device] = "cuda:0", 
+                 cfg: dict = {}) -> None:
+        """State Action Reward State Action (SARSA)
+
+        https://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.17.2539
+        
+        :param models: Models used by the agent
+        :type models: dictionary of skrl.models.torch.Model
+        :param memory: Memory to storage the transitions.
+                       If it is a tuple, the first element will be used for training and 
+                       for the rest only the environment transitions will be added
+        :type memory: skrl.memory.torch.Memory, list of skrl.memory.torch.Memory or None
+        :param observation_space: Observation/state space or shape (default: None)
+        :type observation_space: int, tuple or list of integers, gym.Space or None, optional
+        :param action_space: Action space or shape (default: None)
+        :type action_space: int, tuple or list of integers, gym.Space or None, optional
+        :param device: Computing device (default: "cuda:0")
+        :type device: str or torch.device, optional
+        :param cfg: Configuration dictionary
+        :type cfg: dict
+
+        :raises KeyError: If the models dictionary is missing a required key
+        """
+        _cfg = copy.deepcopy(SARSA_DEFAULT_CONFIG)
+        _cfg.update(cfg)
+        super().__init__(networks=models, 
+                         memory=memory, 
+                         observation_space=observation_space, 
+                         action_space=action_space, 
+                         device=device, 
+                         cfg=_cfg)
+
+        # models
+        if not "policy" in self.networks.keys():
+            raise KeyError("The policy is not defined under 'policy' key (models['policy'])")
+        
+        self.policy = self.networks["policy"]
+
+        # checkpoint networks
+        self.checkpoint_networks = {"policy": self.policy} if self.checkpoint_policy_only else self.networks
+        
+        # configuration
+        self._discount_factor = self.cfg["discount_factor"]
+        
+        self._random_timesteps = self.cfg["random_timesteps"]
+        self._learning_starts = self.cfg["learning_starts"]
+
+        self._epsilon = self.cfg["epsilon"]
+        self._alpha = self.cfg["alpha"]
+
+        # create temporary variables needed for storage and computation
+        self._current_states = None
+        self._current_actions = None
+        self._current_rewards = None
+        self._current_next_states = None
+        self._current_dones = None
+
+    def init(self) -> None:
+        """Initialize the agent
+        """
+        super().init()
+
+    def act(self, 
+            states: torch.Tensor, 
+            timestep: int, 
+            timesteps: int, 
+            inference: bool = False) -> torch.Tensor:
+        """Process the environment's states to make a decision (actions) using the main policy
+
+        :param states: Environment's states
+        :type states: torch.Tensor
+        :param timestep: Current timestep
+        :type timestep: int
+        :param timesteps: Number of timesteps
+        :type timesteps: int
+        :param inference: Flag to indicate whether the network is making inference
+        :type inference: bool
+
+        :return: Actions
+        :rtype: torch.Tensor
+        """
+        # sample random actions
+        actions = self.policy.random_act(states)[0]
+        if timestep < self._random_timesteps:
+            return actions, None, None
+
+        # sample actions with epsilon-greedy policy
+        indexes = (torch.rand(states.shape[0], device=self.device) >= self._epsilon).nonzero().view(-1)
+        if indexes.numel():
+            actions[indexes] = self.policy.act(states[indexes], inference=inference)[0]
+        
+        return actions, None, None
+
+    def record_transition(self, 
+                          states: torch.Tensor, 
+                          actions: torch.Tensor, 
+                          rewards: torch.Tensor, 
+                          next_states: torch.Tensor, 
+                          dones: torch.Tensor, 
+                          timestep: int, 
+                          timesteps: int) -> None:
+        """Record an environment transition in memory
+        
+        :param states: Observations/states of the environment used to make the decision
+        :type states: torch.Tensor
+        :param actions: Actions taken by the agent
+        :type actions: torch.Tensor
+        :param rewards: Instant rewards achieved by the current actions
+        :type rewards: torch.Tensor
+        :param next_states: Next observations/states of the environment
+        :type next_states: torch.Tensor
+        :param dones: Signals to indicate that episodes have ended
+        :type dones: torch.Tensor
+        :param timestep: Current timestep
+        :type timestep: int
+        :param timesteps: Number of timesteps
+        :type timesteps: int
+        """
+        super().record_transition(states, actions, rewards, next_states, dones, timestep, timesteps)
+
+        self._current_states = states
+        self._current_actions = actions
+        self._current_rewards = rewards
+        self._current_next_states = next_states
+        self._current_dones = dones
+
+        if self.memory is not None:
+            self.memory.add_samples(states=states, actions=actions, rewards=rewards, next_states=next_states, dones=dones)
+            for memory in self.secondary_memories:
+                memory.add_samples(states=states, actions=actions, rewards=rewards, next_states=next_states, dones=dones)
+
+    def pre_interaction(self, timestep: int, timesteps: int) -> None:
+        """Callback called before the interaction with the environment
+
+        :param timestep: Current timestep
+        :type timestep: int
+        :param timesteps: Number of timesteps
+        :type timesteps: int
+        """
+        pass
+
+    def post_interaction(self, timestep: int, timesteps: int) -> None:
+        """Callback called after the interaction with the environment
+
+        :param timestep: Current timestep
+        :type timestep: int
+        :param timesteps: Number of timesteps
+        :type timesteps: int
+        """
+        if timestep >= self._learning_starts:
+            self._update(timestep, timesteps)
+
+        # write tracking data and checkpoints
+        super().post_interaction(timestep, timesteps)
+
+    def _update(self, timestep: int, timesteps: int) -> None:
+        """Algorithm's main update step
+
+        :param timestep: Current timestep
+        :type timestep: int
+        :param timesteps: Number of timesteps
+        :type timesteps: int
+        """
+        # compute next actions
+        next_actions = self.policy.act(self._current_next_states)[0]
+
+        # update Q-table
+        q_table = self.policy.table()
+        env_ids = torch.arange(self._current_rewards.shape[0]).view(-1, 1)
+
+        q_table[env_ids, self._current_states, self._current_actions] += self._alpha * (self._current_rewards \
+            + self._discount_factor * self._current_dones.logical_not() \
+                * q_table[env_ids, self._current_next_states, next_actions] \
+                    - q_table[env_ids, self._current_states, self._current_actions])
+        

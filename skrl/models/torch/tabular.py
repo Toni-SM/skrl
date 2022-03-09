69c332b8bed2125123821d24a3b798d9efa49591
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-03-09 16:22:23 +0000

    Save, load and move to device the tabular models

--- skrl/models/torch/tabular.py
+++ skrl/models/torch/tabular.py
@@ -29,18 +29,28 @@ class TabularModel(Model):
         super(TabularModel, self).__init__(observation_space, action_space, device)
 
         self.num_envs = num_envs
-        self.q_table = torch.ones((num_envs, self.num_observations, self.num_actions), dtype=torch.float32, device=self.device)
+
+    def _get_tensor_names(self) -> Tuple[str]:
+        """Get the names of the tensors that the model is using
+
+        :return: Tensor names
+        :rtype: tuple of str
+        """
+        tensors = []
+        for attr in dir(self):
+            if not attr.startswith("__") and issubclass(type(getattr(self, attr)), torch.Tensor):
+                tensors.append(attr)
+        return sorted(tensors)
 
     def act(self, 
             states: torch.Tensor, 
             taken_actions: Union[torch.Tensor, None] = None, 
             inference=False) -> Tuple[torch.Tensor]:
-        """Act deterministically in response to the state of the environment
+        """Act in response to the state of the environment
 
         :param states: Observation/state of the environment used to make the decision
         :type states: torch.Tensor
-        :param taken_actions: Actions taken by a policy to the given states (default: None).
-                              The use of these actions only makes sense in critical models, e.g.
+        :param taken_actions: Actions taken by a policy to the given states (default: None)
         :type taken_actions: torch.Tensor or None, optional
         :param inference: Flag to indicate whether the model is making inference (default: False).
                           If True, the returned tensors will be detached from the current graph
@@ -50,8 +60,8 @@ class TabularModel(Model):
                  The tuple's components are the computed actions and None for the last two components
         :rtype: tuple of torch.Tensor
         """
-        actions = torch.argmax(self.q_table[torch.arange(self.num_envs).view(-1, 1), states], 
-                               dim=-1, keepdim=True).view(-1,1)
+        actions = self.compute(states.to(self.device), 
+                               taken_actions.to(self.device) if taken_actions is not None else taken_actions)
         return actions, None, None
         
     def table(self) -> torch.Tensor:
@@ -61,3 +71,48 @@ class TabularModel(Model):
         :rtype: torch.Tensor
         """
         return self.q_table
+
+    def to(self, *args, **kwargs) -> Model:
+        """Move the model to a different device
+
+        :param args: Arguments to pass to the method
+        :type args: tuple
+        :param kwargs: Keyword arguments to pass to the method
+        :type kwargs: dict
+
+        :return: Model moved to the specified device
+        :rtype: Model
+        """
+        super(TabularModel, self).to(*args, **kwargs)
+        for name in self._get_tensor_names():
+            setattr(self, name, getattr(self, name).to(*args, **kwargs))
+        return self
+
+    def save(self, path: str) -> None:
+        """Save the model to the specified path
+            
+        :param path: Path to save the model to
+        :type path: str
+        """
+        torch.save({name: getattr(self, name) for name in self._get_tensor_names()}, path)
+
+    def load(self, path: str) -> None:
+        """Load the model from the specified path
+        
+        :raises ValueError: If the models are not compatible
+
+        :param path: Path to load the model from
+        :type path: str
+        """
+        tensors = torch.load(path)
+        for name, tensor in tensors.items():
+            if hasattr(self, name) and isinstance(getattr(self, name), torch.Tensor):
+                _tensor = getattr(self, name)
+                if isinstance(_tensor, torch.Tensor):
+                    if _tensor.shape == tensor.shape and _tensor.dtype == tensor.dtype:
+                        setattr(self, name, tensor)
+                    else:
+                        raise ValueError("Tensor shape ({} vs {}) or dtype ({} vs {}) mismatch"\
+                            .format(_tensor.shape, tensor.shape, _tensor.dtype, tensor.dtype))
+            else:
+                raise ValueError("{} is not a tensor of {}".format(name, self.__class__.__name__))

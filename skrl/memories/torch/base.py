a9959014e8f030f455be7d00b80248a051ec9abc
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-05-01 19:04:44 +0200

    Storage multi-samples and compute GAE using the next_values tensor

--- skrl/memories/torch/base.py
+++ skrl/memories/torch/base.py
@@ -209,6 +209,19 @@ class Memory:
                 if name in self.tensors:
                     self.tensors[name][self.memory_index].copy_(tensor)
             self.memory_index += 1
+        # single environment - multi sample (number of environments greater than num_envs (num_envs = 1))
+        elif dim == 2 and self.num_envs == 1:
+            for name, tensor in tensors.items():
+                if name in self.tensors:
+                    num_samples = min(shape[0], self.memory_size - self.memory_index)
+                    remaining_samples = shape[0] - num_samples
+                    # copy the first n samples
+                    self.tensors[name][self.memory_index:self.memory_index + num_samples].copy_(tensor[:num_samples].unsqueeze(dim=1))
+                    self.memory_index += num_samples
+                    # storage remaining samples
+                    if remaining_samples > 0:
+                        self.tensors[name][:remaining_samples].copy_(tensor[num_samples:].unsqueeze(dim=1))
+                        self.memory_index = remaining_samples
         # single environment
         elif dim == 1:
             for name, tensor in tensors.items():
@@ -216,7 +229,7 @@ class Memory:
                     self.tensors[name][self.memory_index, self.env_index].copy_(tensor)
             self.env_index += 1
         else:
-            raise ValueError("Expected tensors with 2-components shape (number of environments, data size), got {}".format(shape))
+            raise ValueError("Expected tensors with 2-components shape (number of environments = {}, data size), got {}".format(self.num_envs, shape))
 
         # update indexes and flags
         if self.env_index >= self.num_envs:
@@ -285,7 +298,18 @@ class Memory:
             return [[self.tensors_view[name][batch] for name in names] for batch in batches]
         return [[self.tensors_view[name] for name in names]]
 
-    def compute_functions(self, states_src: str = "states", actions_src: str = "actions", rewards_src: str = "rewards", next_states_src: str = "next_states", dones_src: str = "dones", values_src: str = "values", returns_dst: Union[str, None] = None, advantages_dst: Union[str, None] = None, last_values: Union[torch.Tensor, None] = None, hyperparameters: dict = {"discount_factor": 0.99, "lambda_coefficient": 0.95, "normalize_returns": False, "normalize_advantages": True}) -> None:
+    def compute_functions(self, 
+                          states_src: str = "states", 
+                          actions_src: str = "actions", 
+                          rewards_src: str = "rewards", 
+                          next_states_src: str = "next_states", 
+                          dones_src: str = "dones", 
+                          values_src: str = "values", 
+                          next_values_src: Union[str, None] = None, 
+                          returns_dst: Union[str, None] = None, 
+                          advantages_dst: Union[str, None] = None, 
+                          last_values: Union[torch.Tensor, None] = None, 
+                          hyperparameters: dict = {"discount_factor": 0.99, "lambda_coefficient": 0.95, "normalize_returns": False, "normalize_advantages": True}) -> None:
         """Compute the following functions for the given tensor names
 
         Available functions:
@@ -304,6 +328,8 @@ class Memory:
         :type dones_src: str, optional
         :param values_src: Name of the tensor containing the values (default: "values")
         :type values_src: str, optional
+        :param next_values_src: Name of the tensor containing the next values (default: None)
+        :type next_values_src: str, optional
         :param returns_dst: Name of the tensor where the returns will be stored (default: None)
         :type returns_dst: str or None, optional
         :param advantages_dst: Name of the tensor where the advantages will be stored (default: None)
@@ -328,8 +354,10 @@ class Memory:
 
         # get source and destination tensors
         rewards = self.tensors[rewards_src]
-        dones = self.tensors[dones_src]
+        not_dones = self.tensors[dones_src].logical_not()
         values = self.tensors[values_src]
+        if next_values_src is not None:
+            next_values = self.tensors[next_values_src]
         
         returns = self.tensors[returns_dst] if returns_dst is not None else torch.zeros_like(rewards)
         advantages = self.tensors[advantages_dst] if advantages_dst is not None else torch.zeros_like(rewards)
@@ -343,10 +371,17 @@ class Memory:
         # compute and normalize the returns
         if returns_dst is not None or advantages_dst is not None:
             advantage = 0
-            for step in reversed(range(self.memory_size)):
-                next_values = values[step + 1] if step < self.memory_size - 1 else last_values
-                advantage = rewards[step] - values[step] + discount_factor * dones[step].logical_not() * (next_values + lambda_coefficient * advantage)
-                returns[step].copy_(advantage + values[step])
+            # use last_values 
+            if next_values_src is None:
+                for step in reversed(range(self.memory_size)):
+                    next_values = values[step + 1] if step < self.memory_size - 1 else last_values
+                    advantage = rewards[step] - values[step] + discount_factor * not_dones[step] * (next_values + lambda_coefficient * advantage)
+                    returns[step].copy_(advantage + values[step])
+            # use next_values
+            else:
+                for step in reversed(range(self.memory_size)):
+                    advantage = rewards[step] - values[step] + discount_factor * next_values[step] +  discount_factor * not_dones[step] * lambda_coefficient * advantage
+                    returns[step].copy_(advantage + values[step])
 
             if normalize_returns:
                 returns.copy_((returns - returns.mean()) / (returns.std() + 1e-8))

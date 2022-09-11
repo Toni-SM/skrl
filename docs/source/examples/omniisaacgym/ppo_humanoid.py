37c08cd4e3f097d5bbc0167bf05611218241d784
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-09-11 16:49:13 +0200

    Update Omniverse Isaac Gym examples

--- docs/source/examples/omniisaacgym/ppo_humanoid.py
+++ docs/source/examples/omniisaacgym/ppo_humanoid.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 # Import the skrl components to build the RL system
-from skrl.models.torch import GaussianModel, DeterministicModel
+from skrl.models.torch import Model, GaussianMixin, DeterministicMixin
 from skrl.memories.torch import RandomMemory
 from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
 from skrl.resources.schedulers.torch import KLAdaptiveRL
@@ -17,14 +17,14 @@ from skrl.utils import set_seed
 set_seed(42)
 
 
-# Define the models (stochastic and deterministic models) for the agent using helper classes.
+# Define the models (stochastic and deterministic models) for the agent using mixins.
 # - Policy: takes as input the environment's observation/state and returns an action
 # - Value: takes the state as input and provides a value to guide the policy
-class Policy(GaussianModel):
+class Policy(GaussianMixin, Model):
     def __init__(self, observation_space, action_space, device, clip_actions=False,
                  clip_log_std=True, min_log_std=-20, max_log_std=2):
-        super().__init__(observation_space, action_space, device, clip_actions,
-                         clip_log_std, min_log_std, max_log_std)
+        Model.__init__(self, observation_space, action_space, device)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std)
 
         self.net = nn.Sequential(nn.Linear(self.num_observations, 400),
                                  nn.ELU(),
@@ -35,12 +35,13 @@ class Policy(GaussianModel):
                                  nn.Linear(100, self.num_actions))
         self.log_std_parameter = nn.Parameter(torch.zeros(self.num_actions))
 
-    def compute(self, states, taken_actions):
+    def compute(self, states, taken_actions, role):
         return self.net(states), self.log_std_parameter
 
-class Value(DeterministicModel):
+class Value(DeterministicMixin, Model):
     def __init__(self, observation_space, action_space, device, clip_actions=False):
-        super().__init__(observation_space, action_space, device, clip_actions)
+        Model.__init__(self, observation_space, action_space, device)
+        DeterministicMixin.__init__(self, clip_actions)
 
         self.net = nn.Sequential(nn.Linear(self.num_observations, 400),
                                  nn.ELU(),
@@ -50,7 +51,7 @@ class Value(DeterministicModel):
                                  nn.ELU(),
                                  nn.Linear(100, 1))
 
-    def compute(self, states, taken_actions):
+    def compute(self, states, taken_actions, role):
         return self.net(states)
 
 
@@ -68,8 +69,9 @@ memory = RandomMemory(memory_size=32, num_envs=env.num_envs, device=device)
 # Instantiate the agent's models (function approximators).
 # PPO requires 2 models, visit its documentation for more details
 # https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#spaces-and-models
-models_ppo = {"policy": Policy(env.observation_space, env.action_space, device),
-              "value": Value(env.observation_space, env.action_space, device)}
+models_ppo = {}
+models_ppo["policy"] = Policy(env.observation_space, env.action_space, device)
+models_ppo["value"] = Value(env.observation_space, env.action_space, device)
 
 # Initialize the models' parameters (weights and biases) using a Gaussian distribution
 for model in models_ppo.values():
@@ -80,7 +82,7 @@ for model in models_ppo.values():
 # Only modify some of the default configuration, visit its documentation to see all the options
 # https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#configuration-and-hyperparameters
 cfg_ppo = PPO_DEFAULT_CONFIG.copy()
-cfg_ppo["rollouts"] = 32
+cfg_ppo["rollouts"] = 32  # memory_size
 cfg_ppo["learning_epochs"] = 5
 cfg_ppo["mini_batches"] = 4  # 32 * 4096 / 32768
 cfg_ppo["discount_factor"] = 0.99

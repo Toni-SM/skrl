Fix tensor dimension when computing parallel variance

skrl/resources/preprocessors/torch/running_standard_scaler.py
@@ -101,7 +101,10 @@ class RunningStandardScaler(nn.Module):
         :type inverse: bool, optional
         """
         if train:
-            self._parallel_variance(torch.mean(x, dim=0), torch.var(x, dim=0), x.shape[0])
+            if x.dim() == 3:
+                self._parallel_variance(torch.mean(x, dim=(0,1)), torch.var(x, dim=(0,1)), x.shape[0] * x.shape[1])
+            else:
+                self._parallel_variance(torch.mean(x, dim=0), torch.var(x, dim=0), x.shape[0])
 
         # scale back the data to the original representation
         if inverse:
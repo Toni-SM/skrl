038f1f1bd0ffd7cbd8de1d0f5b97eed58bfcb838
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-11-14 16:47:15 +0100

    Allow sampling all memory sequentially

--- skrl/memories/torch/base.py
+++ skrl/memories/torch/base.py
@@ -58,6 +58,7 @@ class Memory:
         self.tensors_keep_dimensions = {}
 
         self.sampling_indexes = None
+        self.all_sequence_indexes = np.concatenate([np.arange(i, memory_size * num_envs + i, num_envs) for i in range(num_envs)])
 
         # exporting data
         self.export = export
@@ -346,18 +347,28 @@ class Memory:
             return [[self.tensors_view[name][batch] for name in names] for batch in batches]
         return [[self.tensors_view[name][indexes] for name in names]]
 
-    def sample_all(self, names: Tuple[str], mini_batches: int = 1) -> List[List[torch.Tensor]]:
+    def sample_all(self, names: Tuple[str], mini_batches: int = 1, sequence_length: int = 1) -> List[List[torch.Tensor]]:
         """Sample all data from memory
 
         :param names: Tensors names from which to obtain the samples
         :type names: tuple or list of strings
         :param mini_batches: Number of mini-batches to sample (default: 1)
         :type mini_batches: int, optional
+        :param sequence_length: Length of each sequence (default: 1)
+        :type sequence_length: int, optional
 
         :return: Sampled data from memory.
                  The sampled tensors will have the following shape: (memory size * number of environments, data size)
         :rtype: list of torch.Tensor list
         """
+        # sequential order
+        if sequence_length > 1:
+            if mini_batches > 1:
+                batches = BatchSampler(self.all_sequence_indexes, batch_size=len(self.all_sequence_indexes) // mini_batches, drop_last=True)
+                return [[self.tensors_view[name][batch] for name in names] for batch in batches]
+            return [[self.tensors_view[name][self.all_sequence_indexes] for name in names]]
+
+        # default order
         if mini_batches > 1:
             indexes = np.arange(self.memory_size * self.num_envs)
             batches = BatchSampler(indexes, batch_size=len(indexes) // mini_batches, drop_last=True)

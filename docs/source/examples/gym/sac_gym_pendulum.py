4cccd8de024cb69c885afaf06a745941ccd44c16
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-12-03 19:59:03 +0100

    Rename gym/gymnasium examples and new files

--- docs/source/examples/gym/sac_gym_pendulum.py
+++ docs/source/examples/gym/sac_gym_pendulum.py
@@ -5,32 +5,35 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # Import the skrl components to build the RL system
-from skrl.models.torch import Model, DeterministicMixin
+from skrl.models.torch import Model, DeterministicMixin, GaussianMixin
 from skrl.memories.torch import RandomMemory
-from skrl.agents.torch.ddpg import DDPG, DDPG_DEFAULT_CONFIG
-from skrl.resources.noises.torch import OrnsteinUhlenbeckNoise
+from skrl.agents.torch.sac import SAC, SAC_DEFAULT_CONFIG
 from skrl.trainers.torch import SequentialTrainer
 from skrl.envs.torch import wrap_env
 
 
-# Define the models (deterministic models) for the DDPG agent using mixin
+# Define the models (deterministic models) for the SAC agent using mixin
 # - Actor (policy): takes as input the environment's observation/state and returns an action
 # - Critic: takes the state and action as input and provides a value to guide the policy
-class DeterministicActor(DeterministicMixin, Model):
-    def __init__(self, observation_space, action_space, device, clip_actions=False):
+class Actor(GaussianMixin, Model):
+    def __init__(self, observation_space, action_space, device, clip_actions=False,
+                 clip_log_std=True, min_log_std=-20, max_log_std=2, reduction="sum"):
         Model.__init__(self, observation_space, action_space, device)
-        DeterministicMixin.__init__(self, clip_actions)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
 
         self.linear_layer_1 = nn.Linear(self.num_observations, 400)
         self.linear_layer_2 = nn.Linear(400, 300)
         self.action_layer = nn.Linear(300, self.num_actions)
 
+        self.log_std_parameter = nn.Parameter(torch.zeros(self.num_actions))
+
     def compute(self, inputs, role):
         x = F.relu(self.linear_layer_1(inputs["states"]))
         x = F.relu(self.linear_layer_2(x))
-        return 2 * torch.tanh(self.action_layer(x)), {}  # Pendulum-v1 action_space is -2 to 2
+        # Pendulum-v1 action_space is -2 to 2
+        return 2 * torch.tanh(self.action_layer(x)), self.log_std_parameter, {}
 
-class DeterministicCritic(DeterministicMixin, Model):
+class Critic(DeterministicMixin, Model):
     def __init__(self, observation_space, action_space, device, clip_actions=False):
         Model.__init__(self, observation_space, action_space, device)
         DeterministicMixin.__init__(self, clip_actions)
@@ -59,46 +62,48 @@ device = env.device
 
 
 # Instantiate a RandomMemory (without replacement) as experience replay memory
-memory = RandomMemory(memory_size=15000, num_envs=env.num_envs, device=device, replacement=False)
+memory = RandomMemory(memory_size=20000, num_envs=env.num_envs, device=device, replacement=False)
 
 
 # Instantiate the agent's models (function approximators).
-# DDPG requires 4 models, visit its documentation for more details
-# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ddpg.html#spaces-and-models
-models_ddpg = {}
-models_ddpg["policy"] = DeterministicActor(env.observation_space, env.action_space, device)
-models_ddpg["target_policy"] = DeterministicActor(env.observation_space, env.action_space, device)
-models_ddpg["critic"] = DeterministicCritic(env.observation_space, env.action_space, device)
-models_ddpg["target_critic"] = DeterministicCritic(env.observation_space, env.action_space, device)
+# SAC requires 5 models, visit its documentation for more details
+# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.sac.html#spaces-and-models
+models_sac = {}
+models_sac["policy"] = Actor(env.observation_space, env.action_space, device, clip_actions=True)
+models_sac["critic_1"] = Critic(env.observation_space, env.action_space, device)
+models_sac["critic_2"] = Critic(env.observation_space, env.action_space, device)
+models_sac["target_critic_1"] = Critic(env.observation_space, env.action_space, device)
+models_sac["target_critic_2"] = Critic(env.observation_space, env.action_space, device)
 
 # Initialize the models' parameters (weights and biases) using a Gaussian distribution
-for model in models_ddpg.values():
+for model in models_sac.values():
     model.init_parameters(method_name="normal_", mean=0.0, std=0.1)
 
 
 # Configure and instantiate the agent.
 # Only modify some of the default configuration, visit its documentation to see all the options
-# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ddpg.html#configuration-and-hyperparameters
-cfg_ddpg = DDPG_DEFAULT_CONFIG.copy()
-cfg_ddpg["exploration"]["noise"] = OrnsteinUhlenbeckNoise(theta=0.15, sigma=0.1, base_scale=1.0, device=device)
-cfg_ddpg["batch_size"] = 100
-cfg_ddpg["random_timesteps"] = 100
-cfg_ddpg["learning_starts"] = 100
-# logging to TensorBoard and write checkpoints each 300 and 1500 timesteps respectively
-cfg_ddpg["experiment"]["write_interval"] = 300
-cfg_ddpg["experiment"]["checkpoint_interval"] = 1500
+# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.sac.html#configuration-and-hyperparameters
+cfg_sac = SAC_DEFAULT_CONFIG.copy()
+cfg_sac["discount_factor"] = 0.98
+cfg_sac["batch_size"] = 100
+cfg_sac["random_timesteps"] = 0
+cfg_sac["learning_starts"] = 1000
+cfg_sac["learn_entropy"] = True
+# logging to TensorBoard and write checkpoints each 75 and 750 timesteps respectively
+cfg_sac["experiment"]["write_interval"] = 75
+cfg_sac["experiment"]["checkpoint_interval"] = 750
 
-agent_ddpg = DDPG(models=models_ddpg,
-                  memory=memory,
-                  cfg=cfg_ddpg,
-                  observation_space=env.observation_space,
-                  action_space=env.action_space,
-                  device=device)
+agent_sac = SAC(models=models_sac,
+                memory=memory,
+                cfg=cfg_sac,
+                observation_space=env.observation_space,
+                action_space=env.action_space,
+                device=device)
 
 
 # Configure and instantiate the RL trainer
 cfg_trainer = {"timesteps": 15000, "headless": True}
-trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent_ddpg)
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent_sac)
 
 # start training
 trainer.train()

235421f49c77f264ae4cfdac26a201c3ef15eb4e
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-12-25 23:13:30 +0100

    Add schedulers tests

---
+++ tests/test_resources_schedulers.py
@@ -0,0 +1,18 @@
+import pytest
+
+import torch
+
+from skrl.resources.schedulers.torch import KLAdaptiveRL
+
+
+@pytest.fixture
+def classes_and_kwargs():
+    return [(KLAdaptiveRL, {})]
+
+@pytest.mark.parametrize("optimizer", [torch.optim.Adam([torch.ones((1,))], lr=0.1),
+                                       torch.optim.SGD([torch.ones((1,))], lr=0.1)])
+def test_step(classes_and_kwargs, optimizer):
+    for klass, kwargs in classes_and_kwargs:
+        scheduler = klass(optimizer, **kwargs)
+
+        scheduler.step(0.0)

6c4e91068a0b0f573999ec3c68eac788fbeca026
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-12-25 19:53:13 +0100

    Refactorize noise tests using pytest

---
+++ tests/test_resources_noises.py
@@ -0,0 +1,38 @@
+import pytest
+
+import torch
+
+from skrl.resources.noises.torch import GaussianNoise
+from skrl.resources.noises.torch import OrnsteinUhlenbeckNoise
+
+
+@pytest.fixture
+def noises_and_kwargs():
+    return [(GaussianNoise, {"mean": 0, "std": 1}),
+            (OrnsteinUhlenbeckNoise, {"theta": 0.1, "sigma": 0.2, "base_scale": 0.3})]
+
+
+@pytest.mark.parametrize("device", [None, "cpu", "cuda:0"])
+def test_device(noises_and_kwargs, device):
+    _device = torch.device(device) if device is not None else torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    for klass, kwargs in noises_and_kwargs:
+        noise = klass(device=device, **kwargs)
+
+        output = noise.sample((1,))
+        assert noise.device == _device
+        assert output.device == _device
+
+@pytest.mark.parametrize("size", [(10,), [20, 1], torch.Size([30, 1, 2])])
+def test_sampling(noises_and_kwargs, size):
+    for klass, kwargs in noises_and_kwargs:
+        noise = klass(**kwargs)
+
+        # sample
+        output = noise.sample(size)
+        assert output.size() == torch.Size(size)
+
+        # sample like
+        tensor = torch.rand(size)
+        output = noise.sample_like(tensor)
+        assert output.size() == torch.Size(size)

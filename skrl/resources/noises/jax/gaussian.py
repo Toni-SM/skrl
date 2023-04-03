b505b4cabc8c5f9150094ea45cdabe3264914f37
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-04-03 22:11:30 +0200

    Add just-in-time compilation with XLA for Gaussian noise

--- skrl/resources/noises/jax/gaussian.py
+++ skrl/resources/noises/jax/gaussian.py
@@ -6,7 +6,7 @@ import jax
 import jaxlib
 import jax.numpy as jnp
 
-from skrl import jax_backend
+from skrl import config
 from skrl.resources.noises.jax import Noise
 
 
@@ -29,7 +29,7 @@ class GaussianNoise(Noise):
         super().__init__(device)
 
         # normal distribution
-        if jax_backend == "jax":
+        if config.jax.backend == "jax":
             class _Normal:
                 def __init__(self, loc, scale):
                     self._loc = loc
@@ -43,7 +43,10 @@ class GaussianNoise(Noise):
                     subkey = jax.random.fold_in(self._key, self._i)
                     return jax.random.normal(subkey, size) * self._scale + self._loc
 
-        elif jax_backend == "numpy":
+            # just-in-time compilation with XLA
+            self.sample = jax.jit(self.sample, static_argnames=("size"))
+
+        elif config.jax.backend == "numpy":
             class _Normal:
                 def __init__(self, loc, scale):
                     self._loc = loc

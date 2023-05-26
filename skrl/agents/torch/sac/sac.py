47dad40036aae6bdf2097d1357bd1b13325da567
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-05-26 23:16:54 +0200

    Train the input preprocessor when updating the agent

--- skrl/agents/torch/sac/sac.py
+++ skrl/agents/torch/sac/sac.py
@@ -302,8 +302,9 @@ class SAC(Agent):
         # gradient steps
         for gradient_step in range(self._gradient_steps):
 
-            sampled_states = self._state_preprocessor(sampled_states, train=not gradient_step)
-            sampled_next_states = self._state_preprocessor(sampled_next_states)
+            with torch.no_grad():
+                sampled_states = self._state_preprocessor(sampled_states, train=True)
+                sampled_next_states = self._state_preprocessor(sampled_next_states, train=True)
 
             # compute target values
             with torch.no_grad():

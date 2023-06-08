c25821c338b5417da363bd685a69a553743b4eec
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-06-08 21:08:45 +0200

    Parametrize the PPO optimizer with the grad_norm_clip coefficient

--- skrl/agents/jax/ppo/ppo.py
+++ skrl/agents/jax/ppo/ppo.py
@@ -278,8 +278,8 @@ class PPO(Agent):
                 else:
                     self._learning_rate = self._learning_rate_scheduler(self._learning_rate, **self.cfg["learning_rate_scheduler_kwargs"])
             # optimizer
-            self.policy_optimizer = Adam(model=self.policy, lr=self._learning_rate, scale=scale)
-            self.value_optimizer = Adam(model=self.value, lr=self._learning_rate, scale=scale)
+            self.policy_optimizer = Adam(model=self.policy, lr=self._learning_rate, grad_norm_clip=self._grad_norm_clip, scale=scale)
+            self.value_optimizer = Adam(model=self.value, lr=self._learning_rate, grad_norm_clip=self._grad_norm_clip, scale=scale)
 
             self.checkpoint_modules["policy_optimizer"] = self.policy_optimizer
             self.checkpoint_modules["value_optimizer"] = self.value_optimizer

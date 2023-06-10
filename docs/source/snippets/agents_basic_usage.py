9745ce9fce84e73b4e2f16ecfb6bac214f84666f
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-06-10 12:19:49 +0200

    Add snippets in jax

--- docs/source/snippets/agents_basic_usage.py
+++ docs/source/snippets/agents_basic_usage.py
@@ -1,4 +1,4 @@
-# [start-a2c]
+# [torch-start-a2c]
 # import the agent and its default configuration
 from skrl.agents.torch.a2c import A2C, A2C_DEFAULT_CONFIG
 
@@ -19,10 +19,34 @@ agent = A2C(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-a2c]
+# [torch-end-a2c]
 
 
-# [start-a2c-rnn]
+# [jax-start-a2c]
+# import the agent and its default configuration
+from skrl.agents.jax.a2c import A2C, A2C_DEFAULT_CONFIG
+
+# instantiate the agent's models
+models = {}
+models["policy"] = ...
+models["value"] = ...  # only required during training
+
+# adjust some configuration if necessary
+cfg_agent = A2C_DEFAULT_CONFIG.copy()
+cfg_agent["<KEY>"] = ...
+
+# instantiate the agent
+# (assuming a defined environment <env> and memory <memory>)
+agent = A2C(models=models,
+            memory=memory,  # only required during training
+            cfg=cfg_agent,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=env.device)
+# [jax-end-a2c]
+
+
+# [torch-start-a2c-rnn]
 # import the agent and its default configuration
 from skrl.agents.torch.a2c import A2C_RNN as A2C, A2C_DEFAULT_CONFIG
 
@@ -43,10 +67,10 @@ agent = A2C(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-a2c-rnn]
+# [torch-end-a2c-rnn]
 
 
-# [start-amp]
+# [torch-start-amp]
 # import the agent and its default configuration
 from skrl.agents.torch.amp import AMP, AMP_DEFAULT_CONFIG
 
@@ -75,10 +99,10 @@ agent = AMP(models=models,
             reply_buffer=reply_buffer,
             collect_reference_motions=collect_reference_motions,
             collect_observation=collect_observation)
-# [end-amp]
+# [torch-end-amp]
 
 
-# [start-cem]
+# [torch-start-cem]
 # import the agent and its default configuration
 from skrl.agents.torch.cem import CEM, CEM_DEFAULT_CONFIG
 
@@ -98,10 +122,10 @@ agent = CEM(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-cem]
+# [torch-end-cem]
 
 
-# [start-ddpg]
+# [torch-start-ddpg]
 # import the agent and its default configuration
 from skrl.agents.torch.ddpg import DDPG, DDPG_DEFAULT_CONFIG
 
@@ -124,10 +148,36 @@ agent = DDPG(models=models,
              observation_space=env.observation_space,
              action_space=env.action_space,
              device=env.device)
-# [end-ddpg]
+# [torch-end-ddpg]
 
 
-# [start-ddpg-rnn]
+# [jax-start-ddpg]
+# import the agent and its default configuration
+from skrl.agents.jax.ddpg import DDPG, DDPG_DEFAULT_CONFIG
+
+# instantiate the agent's models
+models = {}
+models["policy"] = ...
+models["target_policy"] = ...  # only required during training
+models["critic"] = ...  # only required during training
+models["target_critic"] = ...  # only required during training
+
+# adjust some configuration if necessary
+cfg_agent = DDPG_DEFAULT_CONFIG.copy()
+cfg_agent["<KEY>"] = ...
+
+# instantiate the agent
+# (assuming a defined environment <env> and memory <memory>)
+agent = DDPG(models=models,
+             memory=memory,  # only required during training
+             cfg=cfg_agent,
+             observation_space=env.observation_space,
+             action_space=env.action_space,
+             device=env.device)
+# [jax-end-ddpg]
+
+
+# [torch-start-ddpg-rnn]
 # import the agent and its default configuration
 from skrl.agents.torch.ddpg import DDPG_RNN as DDPG, DDPG_DEFAULT_CONFIG
 
@@ -150,10 +200,10 @@ agent = DDPG(models=models,
              observation_space=env.observation_space,
              action_space=env.action_space,
              device=env.device)
-# [end-ddpg-rnn]
+# [torch-end-ddpg-rnn]
 
 
-# [start-ddqn]
+# [torch-start-ddqn]
 # import the agent and its default configuration
 from skrl.agents.torch.dqn import DDQN, DDQN_DEFAULT_CONFIG
 
@@ -174,10 +224,10 @@ agent = DDQN(models=models,
              observation_space=env.observation_space,
              action_space=env.action_space,
              device=env.device)
-# [end-ddqn]
+# [torch-end-ddqn]
 
 
-# [start-dqn]
+# [torch-start-dqn]
 # import the agent and its default configuration
 from skrl.agents.torch.dqn import DQN, DQN_DEFAULT_CONFIG
 
@@ -198,10 +248,10 @@ agent = DQN(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-dqn]
+# [torch-end-dqn]
 
 
-# [start-ppo]
+# [torch-start-ppo]
 # import the agent and its default configuration
 from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
 
@@ -222,10 +272,34 @@ agent = PPO(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-ppo]
+# [torch-end-ppo]
 
 
-# [start-ppo-rnn]
+# [jax-start-ppo]
+# import the agent and its default configuration
+from skrl.agents.jax.ppo import PPO, PPO_DEFAULT_CONFIG
+
+# instantiate the agent's models
+models = {}
+models["policy"] = ...
+models["value"] = ...  # only required during training
+
+# adjust some configuration if necessary
+cfg_agent = PPO_DEFAULT_CONFIG.copy()
+cfg_agent["<KEY>"] = ...
+
+# instantiate the agent
+# (assuming a defined environment <env> and memory <memory>)
+agent = PPO(models=models,
+            memory=memory,  # only required during training
+            cfg=cfg_agent,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=env.device)
+# [jax-end-ppo]
+
+
+# [torch-start-ppo-rnn]
 # import the agent and its default configuration
 from skrl.agents.torch.ppo import PPO_RNN as PPO, PPO_DEFAULT_CONFIG
 
@@ -246,10 +320,10 @@ agent = PPO(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-ppo-rnn]
+# [torch-end-ppo-rnn]
 
 
-# [start-q-learning]
+# [torch-start-q-learning]
 # import the agent and its default configuration
 from skrl.agents.torch.q_learning import Q_LEARNING, Q_LEARNING_DEFAULT_CONFIG
 
@@ -269,10 +343,10 @@ agent = Q_LEARNING(models=models,
                    observation_space=env.observation_space,
                    action_space=env.action_space,
                    device=env.device)
-# [end-q-learning]
+# [torch-end-q-learning]
 
 
-# [start-rpo]
+# [torch-start-rpo]
 # import the agent and its default configuration
 from skrl.agents.torch.rpo import RPO, RPO_DEFAULT_CONFIG
 
@@ -293,10 +367,34 @@ agent = RPO(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-rpo]
+# [torch-end-rpo]
 
 
-# [start-rpo-rnn]
+# [jax-start-rpo]
+# import the agent and its default configuration
+from skrl.agents.jax.rpo import RPO, RPO_DEFAULT_CONFIG
+
+# instantiate the agent's models
+models = {}
+models["policy"] = ...
+models["value"] = ...  # only required during training
+
+# adjust some configuration if necessary
+cfg_agent = RPO_DEFAULT_CONFIG.copy()
+cfg_agent["<KEY>"] = ...
+
+# instantiate the agent
+# (assuming a defined environment <env> and memory <memory>)
+agent = RPO(models=models,
+            memory=memory,  # only required during training
+            cfg=cfg_agent,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=env.device)
+# [jax-end-rpo]
+
+
+# [torch-start-rpo-rnn]
 # import the agent and its default configuration
 from skrl.agents.torch.rpo import RPO_RNN as RPO, RPO_DEFAULT_CONFIG
 
@@ -317,10 +415,10 @@ agent = RPO(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-rpo-rnn]
+# [torch-end-rpo-rnn]
 
 
-# [start-sac]
+# [torch-start-sac]
 # import the agent and its default configuration
 from skrl.agents.torch.sac import SAC, SAC_DEFAULT_CONFIG
 
@@ -344,10 +442,37 @@ agent = SAC(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-sac]
+# [torch-end-sac]
 
 
-# [start-sac-rnn]
+# [jax-start-sac]
+# import the agent and its default configuration
+from skrl.agents.jax.sac import SAC, SAC_DEFAULT_CONFIG
+
+# instantiate the agent's models
+models = {}
+models["policy"] = ...
+models["critic_1"] = ...  # only required during training
+models["critic_2"] = ...  # only required during training
+models["target_critic_1"] = ...  # only required during training
+models["target_critic_2"] = ...  # only required during training
+
+# adjust some configuration if necessary
+cfg_agent = SAC_DEFAULT_CONFIG.copy()
+cfg_agent["<KEY>"] = ...
+
+# instantiate the agent
+# (assuming a defined environment <env> and memory <memory>)
+agent = SAC(models=models,
+            memory=memory,  # only required during training
+            cfg=cfg_agent,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=env.device)
+# [jax-end-sac]
+
+
+# [torch-start-sac-rnn]
 # import the agent and its default configuration
 from skrl.agents.torch.sac import SAC_RNN as SAC, SAC_DEFAULT_CONFIG
 
@@ -371,10 +496,10 @@ agent = SAC(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-sac-rnn]
+# [torch-end-sac-rnn]
 
 
-# [start-sarsa]
+# [torch-start-sarsa]
 # import the agent and its default configuration
 from skrl.agents.torch.sarsa import SARSA, SARSA_DEFAULT_CONFIG
 
@@ -394,10 +519,10 @@ agent = SARSA(models=models,
               observation_space=env.observation_space,
               action_space=env.action_space,
               device=env.device)
-# [end-sarsa]
+# [torch-end-sarsa]
 
 
-# [start-td3]
+# [torch-start-td3]
 # import the agent and its default configuration
 from skrl.agents.torch.td3 import TD3, TD3_DEFAULT_CONFIG
 
@@ -422,10 +547,38 @@ agent = TD3(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-td3]
+# [torch-end-td3]
 
 
-# [start-td3-rnn]
+# [jax-start-td3]
+# import the agent and its default configuration
+from skrl.agents.jax.td3 import TD3, TD3_DEFAULT_CONFIG
+
+# instantiate the agent's models
+models = {}
+models["policy"] = ...
+models["target_policy"] = ...  # only required during training
+models["critic_1"] = ...  # only required during training
+models["critic_2"] = ...  # only required during training
+models["target_critic_1"] = ...  # only required during training
+models["target_critic_2"] = ...  # only required during training
+
+# adjust some configuration if necessary
+cfg_agent = TD3_DEFAULT_CONFIG.copy()
+cfg_agent["<KEY>"] = ...
+
+# instantiate the agent
+# (assuming a defined environment <env> and memory <memory>)
+agent = TD3(models=models,
+            memory=memory,  # only required during training
+            cfg=cfg_agent,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=env.device)
+# [jax-end-td3]
+
+
+# [torch-start-td3-rnn]
 # import the agent and its default configuration
 from skrl.agents.torch.td3 import TD3_RNN as TD3, TD3_DEFAULT_CONFIG
 
@@ -450,10 +603,10 @@ agent = TD3(models=models,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=env.device)
-# [end-td3-rnn]
+# [torch-end-td3-rnn]
 
 
-# [start-trpo]
+# [torch-start-trpo]
 # import the agent and its default configuration
 from skrl.agents.torch.trpo import TRPO, TRPO_DEFAULT_CONFIG
 
@@ -474,10 +627,10 @@ agent = TRPO(models=models,
              observation_space=env.observation_space,
              action_space=env.action_space,
              device=env.device)
-# [end-trpo]
+# [torch-end-trpo]
 
 
-# [start-trpo-rnn]
+# [torch-start-trpo-rnn]
 # import the agent and its default configuration
 from skrl.agents.torch.trpo import TRPO_RNN as TRPO, TRPO_DEFAULT_CONFIG
 
@@ -498,4 +651,4 @@ agent = TRPO(models=models,
              observation_space=env.observation_space,
              action_space=env.action_space,
              device=env.device)
-# [end-trpo-rnn]
+# [torch-end-trpo-rnn]

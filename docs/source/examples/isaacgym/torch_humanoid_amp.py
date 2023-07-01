15723eb26156e845442c3906c96aca1732466f7a
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-07-01 14:20:55 +0200

    Update IsaacGymEnvs examples in torch

--- docs/source/examples/isaacgym/torch_humanoid_amp.py
+++ docs/source/examples/isaacgym/torch_humanoid_amp.py
@@ -3,7 +3,7 @@ import isaacgym
 import torch
 import torch.nn as nn
 
-# Import the skrl components to build the RL system
+# import the skrl components to build the RL system
 from skrl.models.torch import Model, GaussianMixin, DeterministicMixin
 from skrl.memories.torch import RandomMemory
 from skrl.agents.torch.amp import AMP, AMP_DEFAULT_CONFIG
@@ -14,11 +14,11 @@ from skrl.envs.torch import load_isaacgym_env_preview4
 from skrl.utils import set_seed
 
 
-# set the seed for reproducibility
-set_seed(42)
+# seed for reproducibility
+set_seed()
 
 
-# Define the models (stochastic and deterministic models) for the agent using mixins.
+# define models (stochastic and deterministic models) using mixins
 # - Policy: takes as input the environment's observation/state and returns an action
 # - Value: takes the state as input and provides a value to guide the policy
 # - Discriminator: differentiate between police-generated behaviors and behaviors from the motion dataset
@@ -69,66 +69,66 @@ class Discriminator(DeterministicMixin, Model):
         return self.net(inputs["states"]), {}
 
 
-# Load and wrap the Isaac Gym environment
-env = load_isaacgym_env_preview4(task_name="HumanoidAMP")   # preview 3 and 4 use the same loader
+# load and wrap the Isaac Gym environment
+env = load_isaacgym_env_preview4(task_name="HumanoidAMP")
 env = wrap_env(env)
 
 device = env.device
 
 
-# Instantiate a RandomMemory as rollout buffer (any memory can be used for this)
+# instantiate a memory as rollout buffer (any memory can be used for this)
 memory = RandomMemory(memory_size=16, num_envs=env.num_envs, device=device)
 
 
-# Instantiate the agent's models (function approximators).
+# instantiate the agent's models (function approximators).
 # AMP requires 3 models, visit its documentation for more details
-# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.amp.html#spaces-and-models
-models_amp = {}
-models_amp["policy"] = Policy(env.observation_space, env.action_space, device)
-models_amp["value"] = Value(env.observation_space, env.action_space, device)
-models_amp["discriminator"] = Discriminator(env.amp_observation_space, env.action_space, device)
+# https://skrl.readthedocs.io/en/latest/api/agents/amp.html#models
+models = {}
+models["policy"] = Policy(env.observation_space, env.action_space, device)
+models["value"] = Value(env.observation_space, env.action_space, device)
+models["discriminator"] = Discriminator(env.amp_observation_space, env.action_space, device)
 
 
-# Configure and instantiate the agent.
-# Only modify some of the default configuration, visit its documentation to see all the options
-# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.amp.html#configuration-and-hyperparameters
-cfg_amp = AMP_DEFAULT_CONFIG.copy()
-cfg_amp["rollouts"] = 16  # memory_size
-cfg_amp["learning_epochs"] = 6
-cfg_amp["mini_batches"] = 2  # 16 * 4096 / 32768
-cfg_amp["discount_factor"] = 0.99
-cfg_amp["lambda"] = 0.95
-cfg_amp["learning_rate"] = 5e-5
-cfg_amp["random_timesteps"] = 0
-cfg_amp["learning_starts"] = 0
-cfg_amp["grad_norm_clip"] = 0.0
-cfg_amp["ratio_clip"] = 0.2
-cfg_amp["value_clip"] = 0.2
-cfg_amp["clip_predicted_values"] = False
-cfg_amp["entropy_loss_scale"] = 0.0
-cfg_amp["value_loss_scale"] = 2.5
-cfg_amp["discriminator_loss_scale"] = 5.0
-cfg_amp["amp_batch_size"] = 512
-cfg_amp["task_reward_weight"] = 0.0
-cfg_amp["style_reward_weight"] = 1.0
-cfg_amp["discriminator_batch_size"] = 4096
-cfg_amp["discriminator_reward_scale"] = 2
-cfg_amp["discriminator_logit_regularization_scale"] = 0.05
-cfg_amp["discriminator_gradient_penalty_scale"] = 5
-cfg_amp["discriminator_weight_decay_scale"] = 0.0001
-cfg_amp["state_preprocessor"] = RunningStandardScaler
-cfg_amp["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
-cfg_amp["value_preprocessor"] = RunningStandardScaler
-cfg_amp["value_preprocessor_kwargs"] = {"size": 1, "device": device}
-cfg_amp["amp_state_preprocessor"] = RunningStandardScaler
-cfg_amp["amp_state_preprocessor_kwargs"] = {"size": env.amp_observation_space, "device": device}
-# logging to TensorBoard and write checkpoints each 16 and 4000 timesteps respectively
-cfg_amp["experiment"]["write_interval"] = 160
-cfg_amp["experiment"]["checkpoint_interval"] = 4000
+# configure and instantiate the agent (visit its documentation to see all the options)
+# https://skrl.readthedocs.io/en/latest/api/agents/amp.html#configuration-and-hyperparameters
+cfg = AMP_DEFAULT_CONFIG.copy()
+cfg["rollouts"] = 16  # memory_size
+cfg["learning_epochs"] = 6
+cfg["mini_batches"] = 2  # 16 * 4096 / 32768
+cfg["discount_factor"] = 0.99
+cfg["lambda"] = 0.95
+cfg["learning_rate"] = 5e-5
+cfg["random_timesteps"] = 0
+cfg["learning_starts"] = 0
+cfg["grad_norm_clip"] = 0
+cfg["ratio_clip"] = 0.2
+cfg["value_clip"] = 0.2
+cfg["clip_predicted_values"] = False
+cfg["entropy_loss_scale"] = 0.0
+cfg["value_loss_scale"] = 2.5
+cfg["discriminator_loss_scale"] = 5.0
+cfg["amp_batch_size"] = 512
+cfg["task_reward_weight"] = 0.0
+cfg["style_reward_weight"] = 1.0
+cfg["discriminator_batch_size"] = 4096
+cfg["discriminator_reward_scale"] = 2
+cfg["discriminator_logit_regularization_scale"] = 0.05
+cfg["discriminator_gradient_penalty_scale"] = 5
+cfg["discriminator_weight_decay_scale"] = 0.0001
+cfg["state_preprocessor"] = RunningStandardScaler
+cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
+cfg["value_preprocessor"] = RunningStandardScaler
+cfg["value_preprocessor_kwargs"] = {"size": 1, "device": device}
+cfg["amp_state_preprocessor"] = RunningStandardScaler
+cfg["amp_state_preprocessor_kwargs"] = {"size": env.amp_observation_space, "device": device}
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 160
+cfg["experiment"]["checkpoint_interval"] = 4000
+cfg["experiment"]["directory"] = "runs/torch/HumanoidAMP"
 
-agent = AMP(models=models_amp,
+agent = AMP(models=models,
             memory=memory,
-            cfg=cfg_amp,
+            cfg=cfg,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=device,
@@ -139,7 +139,7 @@ agent = AMP(models=models_amp,
             collect_observation=lambda: env.reset_done()[0]["obs"])
 
 
-# Configure and instantiate the RL trainer
+# configure and instantiate the RL trainer
 cfg_trainer = {"timesteps": 80000, "headless": True}
 trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 
--- docs/source/examples/isaacgym/torch_humanoid_ppo.py
+++ docs/source/examples/isaacgym/torch_humanoid_ppo.py
@@ -3,7 +3,7 @@ import isaacgym
 import torch
 import torch.nn as nn
 
-# Import the skrl components to build the RL system
+# import the skrl components to build the RL system
 from skrl.models.torch import Model, GaussianMixin, DeterministicMixin
 from skrl.memories.torch import RandomMemory
 from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
@@ -15,11 +15,11 @@ from skrl.envs.torch import load_isaacgym_env_preview4
 from skrl.utils import set_seed
 
 
-# set the seed for reproducibility
-set_seed(42)
+# seed for reproducibility
+set_seed()
 
 
-# Define the shared model (stochastic and deterministic models) for the agent using mixins.
+# define shared model (stochastic and deterministic models) using mixins
 class Shared(GaussianMixin, DeterministicMixin, Model):
     def __init__(self, observation_space, action_space, device, clip_actions=False,
                  clip_log_std=True, min_log_std=-20, max_log_std=2, reduction="sum"):
@@ -52,64 +52,64 @@ class Shared(GaussianMixin, DeterministicMixin, Model):
             return self.value_layer(self.net(inputs["states"])), {}
 
 
-# Load and wrap the Isaac Gym environment
-env = load_isaacgym_env_preview4(task_name="Humanoid")   # preview 3 and 4 use the same loader
+# load and wrap the Isaac Gym environment
+env = load_isaacgym_env_preview4(task_name="Humanoid")
 env = wrap_env(env)
 
 device = env.device
 
 
-# Instantiate a RandomMemory as rollout buffer (any memory can be used for this)
+# instantiate a memory as rollout buffer (any memory can be used for this)
 memory = RandomMemory(memory_size=32, num_envs=env.num_envs, device=device)
 
 
-# Instantiate the agent's models (function approximators).
+# instantiate the agent's models (function approximators).
 # PPO requires 2 models, visit its documentation for more details
-# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#spaces-and-models
-models_ppo = {}
-models_ppo["policy"] = Shared(env.observation_space, env.action_space, device)
-models_ppo["value"] = models_ppo["policy"]  # same instance: shared model
+# https://skrl.readthedocs.io/en/latest/api/agents/ppo.html#models
+models = {}
+models["policy"] = Shared(env.observation_space, env.action_space, device)
+models["value"] = models["policy"]  # same instance: shared model
 
 
-# Configure and instantiate the agent.
-# Only modify some of the default configuration, visit its documentation to see all the options
-# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#configuration-and-hyperparameters
-cfg_ppo = PPO_DEFAULT_CONFIG.copy()
-cfg_ppo["rollouts"] = 32  # memory_size
-cfg_ppo["learning_epochs"] = 5
-cfg_ppo["mini_batches"] = 4  # 32 * 4096 / 32768
-cfg_ppo["discount_factor"] = 0.99
-cfg_ppo["lambda"] = 0.95
-cfg_ppo["learning_rate"] = 5e-4
-cfg_ppo["learning_rate_scheduler"] = KLAdaptiveRL
-cfg_ppo["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
-cfg_ppo["random_timesteps"] = 0
-cfg_ppo["learning_starts"] = 0
-cfg_ppo["grad_norm_clip"] = 1.0
-cfg_ppo["ratio_clip"] = 0.2
-cfg_ppo["value_clip"] = 0.2
-cfg_ppo["clip_predicted_values"] = True
-cfg_ppo["entropy_loss_scale"] = 0.0
-cfg_ppo["value_loss_scale"] = 2.0
-cfg_ppo["kl_threshold"] = 0
-cfg_ppo["rewards_shaper"] = lambda rewards, timestep, timesteps: rewards * 0.01
-cfg_ppo["state_preprocessor"] = RunningStandardScaler
-cfg_ppo["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
-cfg_ppo["value_preprocessor"] = RunningStandardScaler
-cfg_ppo["value_preprocessor_kwargs"] = {"size": 1, "device": device}
-# logging to TensorBoard and write checkpoints each 160 and 1600 timesteps respectively
-cfg_ppo["experiment"]["write_interval"] = 160
-cfg_ppo["experiment"]["checkpoint_interval"] = 1600
+# configure and instantiate the agent (visit its documentation to see all the options)
+# https://skrl.readthedocs.io/en/latest/api/agents/ppo.html#configuration-and-hyperparameters
+cfg = PPO_DEFAULT_CONFIG.copy()
+cfg["rollouts"] = 32  # memory_size
+cfg["learning_epochs"] = 5
+cfg["mini_batches"] = 4  # 32 * 4096 / 32768
+cfg["discount_factor"] = 0.99
+cfg["lambda"] = 0.95
+cfg["learning_rate"] = 5e-4
+cfg["learning_rate_scheduler"] = KLAdaptiveRL
+cfg["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
+cfg["random_timesteps"] = 0
+cfg["learning_starts"] = 0
+cfg["grad_norm_clip"] = 1.0
+cfg["ratio_clip"] = 0.2
+cfg["value_clip"] = 0.2
+cfg["clip_predicted_values"] = True
+cfg["entropy_loss_scale"] = 0.0
+cfg["value_loss_scale"] = 2.0
+cfg["kl_threshold"] = 0
+cfg["rewards_shaper"] = lambda rewards, timestep, timesteps: rewards * 0.01
+cfg["state_preprocessor"] = RunningStandardScaler
+cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
+cfg["value_preprocessor"] = RunningStandardScaler
+cfg["value_preprocessor_kwargs"] = {"size": 1, "device": device}
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 160
+cfg["experiment"]["checkpoint_interval"] = 1600
+cfg["experiment"]["directory"] = "runs/torch/Humanoid"
 
-agent = PPO(models=models_ppo,
+agent = PPO(models=models,
             memory=memory,
-            cfg=cfg_ppo,
+            cfg=cfg,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=device)
 
 
-# Configure and instantiate the RL trainer
+# configure and instantiate the RL trainer
 cfg_trainer = {"timesteps": 32000, "headless": True}
 trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 

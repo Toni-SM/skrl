202d7384db052b027e354758a6a55d611f0d72a9
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-07-15 15:47:38 +0200

    Add DDPG/TD3/SAC examples for Isaac Gym

---
+++ docs/source/examples/isaacgym/jax_ant_sac.py
@@ -0,0 +1,118 @@
+import isaacgym
+
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
+
+# import the skrl components to build the RL system
+from skrl.models.jax import Model, GaussianMixin, DeterministicMixin
+from skrl.memories.jax import RandomMemory
+from skrl.agents.jax.sac import SAC, SAC_DEFAULT_CONFIG
+from skrl.resources.preprocessors.jax import RunningStandardScaler
+from skrl.trainers.jax import SequentialTrainer
+from skrl.envs.jax import wrap_env
+from skrl.envs.jax import load_isaacgym_env_preview4
+from skrl.utils import set_seed
+from skrl import config
+
+config.jax.backend = "jax"  # or "numpy"
+
+
+# seed for reproducibility
+set_seed()
+
+
+# define models (stochastic and deterministic models) using mixins
+class StochasticActor(GaussianMixin, Model):
+    def __init__(self, observation_space, action_space, device=None, clip_actions=False,
+                 clip_log_std=True, min_log_std=-5, max_log_std=2, reduction="sum", **kwargs):
+        Model.__init__(self, observation_space, action_space, device, **kwargs)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
+
+    @nn.compact  # marks the given module method allowing inlined submodules
+    def __call__(self, inputs, role):
+        x = nn.relu(nn.Dense(512)(inputs["states"]))
+        x = nn.relu(nn.Dense(256)(x))
+        x = nn.Dense(self.num_actions)(x)
+        log_std = self.param("log_std", lambda _: jnp.zeros(self.num_actions))
+        return nn.tanh(x), log_std, {}
+
+class Critic(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device=None, clip_actions=False, **kwargs):
+        Model.__init__(self, observation_space, action_space, device, **kwargs)
+        DeterministicMixin.__init__(self, clip_actions)
+
+    @nn.compact  # marks the given module method allowing inlined submodules
+    def __call__(self, inputs, role):
+        x = jnp.concatenate([inputs["states"], inputs["taken_actions"]], axis=-1)
+        x = nn.relu(nn.Dense(512)(x))
+        x = nn.relu(nn.Dense(256)(x))
+        x = nn.Dense(1)(x)
+        return x, {}
+
+
+# load and wrap the Isaac Gym environment
+env = load_isaacgym_env_preview4(task_name="Ant", num_envs=64)
+env = wrap_env(env)
+
+device = env.device
+
+
+# instantiate a memory as experience replay
+memory = RandomMemory(memory_size=15625, num_envs=env.num_envs, device=device)
+
+
+# instantiate the agent's models (function approximators).
+# SAC requires 5 models, visit its documentation for more details
+# https://skrl.readthedocs.io/en/latest/api/agents/sac.html#models
+models = {}
+models["policy"] = StochasticActor(env.observation_space, env.action_space, device, clip_actions=True)
+models["critic_1"] = Critic(env.observation_space, env.action_space, device)
+models["critic_2"] = Critic(env.observation_space, env.action_space, device)
+models["target_critic_1"] = Critic(env.observation_space, env.action_space, device)
+models["target_critic_2"] = Critic(env.observation_space, env.action_space, device)
+
+key = jax.random.PRNGKey(0)
+models["policy"].init_state_dict(key, {"states": env.observation_space.sample()}, "policy")
+models["critic_1"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "critic_1")
+models["critic_2"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "critic_2")
+models["target_critic_1"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "target_critic_1")
+models["target_critic_2"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "target_critic_2")
+
+
+# configure and instantiate the agent (visit its documentation to see all the options)
+# https://skrl.readthedocs.io/en/latest/api/agents/sac.html#configuration-and-hyperparameters
+cfg = SAC_DEFAULT_CONFIG.copy()
+cfg["gradient_steps"] = 1
+cfg["batch_size"] = 4096
+cfg["discount_factor"] = 0.99
+cfg["polyak"] = 0.005
+cfg["actor_learning_rate"] = 5e-4
+cfg["critic_learning_rate"] = 5e-4
+cfg["random_timesteps"] = 80
+cfg["learning_starts"] = 80
+cfg["grad_norm_clip"] = 0
+cfg["learn_entropy"] = True
+cfg["entropy_learning_rate"] = 5e-3
+cfg["initial_entropy_value"] = 1.0
+cfg["state_preprocessor"] = RunningStandardScaler
+cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 800
+cfg["experiment"]["checkpoint_interval"] = 8000
+cfg["experiment"]["directory"] = "runs/jax/Ant"
+
+agent = SAC(models=models,
+            memory=memory,
+            cfg=cfg,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=device)
+
+
+# configure and instantiate the RL trainer
+cfg_trainer = {"timesteps": 160000, "headless": True}
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
+
+# start training
+trainer.train()
---
+++ docs/source/examples/isaacgym/jax_ant_td3.py
@@ -0,0 +1,118 @@
+import isaacgym
+
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
+
+# import the skrl components to build the RL system
+from skrl.models.jax import Model, DeterministicMixin
+from skrl.memories.jax import RandomMemory
+from skrl.agents.jax.td3 import TD3, TD3_DEFAULT_CONFIG
+from skrl.resources.preprocessors.jax import RunningStandardScaler
+from skrl.resources.noises.jax import GaussianNoise
+from skrl.trainers.jax import SequentialTrainer
+from skrl.envs.jax import wrap_env
+from skrl.envs.jax import load_isaacgym_env_preview4
+from skrl.utils import set_seed
+from skrl import config
+
+config.jax.backend = "jax"  # or "numpy"
+
+
+# seed for reproducibility
+set_seed()
+
+
+# define models (deterministic models) using mixins
+class DeterministicActor(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device=None, clip_actions=False, **kwargs):
+        Model.__init__(self, observation_space, action_space, device, **kwargs)
+        DeterministicMixin.__init__(self, clip_actions)
+
+    @nn.compact
+    def __call__(self, inputs, role):
+        x = nn.relu(nn.Dense(512)(inputs["states"]))
+        x = nn.relu(nn.Dense(256)(x))
+        x = nn.Dense(self.num_actions)(x)
+        return nn.tanh(x), {}
+
+class Critic(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device=None, clip_actions=False, **kwargs):
+        Model.__init__(self, observation_space, action_space, device, **kwargs)
+        DeterministicMixin.__init__(self, clip_actions)
+
+    @nn.compact  # marks the given module method allowing inlined submodules
+    def __call__(self, inputs, role):
+        x = jnp.concatenate([inputs["states"], inputs["taken_actions"]], axis=-1)
+        x = nn.relu(nn.Dense(512)(x))
+        x = nn.relu(nn.Dense(256)(x))
+        x = nn.Dense(1)(x)
+        return x, {}
+
+
+# load and wrap the Isaac Gym environment
+env = load_isaacgym_env_preview4(task_name="Ant", num_envs=64)
+env = wrap_env(env)
+
+device = env.device
+
+
+# instantiate a memory as experience replay
+memory = RandomMemory(memory_size=15625, num_envs=env.num_envs, device=device)
+
+
+# instantiate the agent's models (function approximators).
+# TD3 requires 6 models, visit its documentation for more details
+# https://skrl.readthedocs.io/en/latest/api/agents/td3.html#models
+models = {}
+models["policy"] = DeterministicActor(env.observation_space, env.action_space, device)
+models["target_policy"] = DeterministicActor(env.observation_space, env.action_space, device)
+models["critic_1"] = Critic(env.observation_space, env.action_space, device)
+models["critic_2"] = Critic(env.observation_space, env.action_space, device)
+models["target_critic_1"] = Critic(env.observation_space, env.action_space, device)
+models["target_critic_2"] = Critic(env.observation_space, env.action_space, device)
+
+key = jax.random.PRNGKey(0)
+models["policy"].init_state_dict(key, {"states": env.observation_space.sample()}, "policy")
+models["target_policy"].init_state_dict(key, {"states": env.observation_space.sample()}, "target_policy")
+models["critic_1"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "critic_1")
+models["critic_2"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "critic_2")
+models["target_critic_1"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "target_critic_1")
+models["target_critic_2"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "target_critic_2")
+
+
+# configure and instantiate the agent (visit its documentation to see all the options)
+# https://skrl.readthedocs.io/en/latest/api/agents/td3.html#configuration-and-hyperparameters
+cfg = TD3_DEFAULT_CONFIG.copy()
+cfg["exploration"]["noise"] = GaussianNoise(0, 0.1, device=device)
+cfg["smooth_regularization_noise"] = GaussianNoise(0, 0.1, device=device)
+cfg["smooth_regularization_clip"] = 0.5
+cfg["gradient_steps"] = 1
+cfg["batch_size"] = 4096
+cfg["discount_factor"] = 0.99
+cfg["polyak"] = 0.005
+cfg["actor_learning_rate"] = 5e-4
+cfg["critic_learning_rate"] = 5e-4
+cfg["random_timesteps"] = 80
+cfg["learning_starts"] = 80
+cfg["state_preprocessor"] = RunningStandardScaler
+cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 800
+cfg["experiment"]["checkpoint_interval"] = 8000
+cfg["experiment"]["directory"] = "runs/jax/Ant"
+
+agent = TD3(models=models,
+            memory=memory,
+            cfg=cfg,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=device)
+
+
+# configure and instantiate the RL trainer
+cfg_trainer = {"timesteps": 160000, "headless": True}
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
+
+# start training
+trainer.train()

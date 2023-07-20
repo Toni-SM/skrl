f0af85470c3b2b81947d121616796fb7462bf22f
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-07-20 23:32:30 +0200

    Add off-policy examples in torch and jax

---
+++ docs/source/examples/omniisaacgym/jax_ant_sac.py
@@ -0,0 +1,135 @@
+"""
+Notes for Isaac Sim 2022.2.1 or earlier (Python 3.7 environment):
+  * Python 3.7 is only supported up to jax<=0.3.25.
+    See: https://github.com/google/jax/blob/main/CHANGELOG.md#jaxlib-041-dec-13-2022.
+  * Builds for jaxlib<=0.3.25 are only available up to NVIDIA CUDA 11 and cuDNN 8.2 versions.
+    See: https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+    and search for `cuda11/jaxlib-0.3.25+cuda11.cudnn82-cp37-cp37m-manylinux2014_x86_64.whl`.
+  * The `jax.Device = jax.xla.Device` statement is required by skrl to support jax<0.4.3.
+  * Models require overloading the `__hash__` method to avoid "TypeError: Failed to hash Flax Module".
+"""
+
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+
+jax.Device = jax.xla.Device  # for Isaac Sim 2022.2.1 or earlier
+
+# import the skrl components to build the RL system
+from skrl import config
+from skrl.agents.jax.sac import SAC, SAC_DEFAULT_CONFIG
+from skrl.envs.jax import load_omniverse_isaacgym_env, wrap_env
+from skrl.memories.jax import RandomMemory
+from skrl.models.jax import DeterministicMixin, GaussianMixin, Model
+from skrl.resources.preprocessors.jax import RunningStandardScaler
+from skrl.trainers.jax import SequentialTrainer
+from skrl.utils import set_seed
+
+
+config.jax.backend = "jax"  # or "numpy"
+
+
+# seed for reproducibility
+set_seed()  # e.g. `set_seed(42)` for fixed seed
+
+
+# define models (stochastic and deterministic models) using mixins
+class StochasticActor(GaussianMixin, Model):
+    def __init__(self, observation_space, action_space, device=None, clip_actions=False,
+                 clip_log_std=True, min_log_std=-5, max_log_std=2, reduction="sum", **kwargs):
+        Model.__init__(self, observation_space, action_space, device, **kwargs)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
+
+    def __hash__(self):  # for Isaac Sim 2022.2.1 or earlier
+        return id(self)
+
+    @nn.compact  # marks the given module method allowing inlined submodules
+    def __call__(self, inputs, role):
+        x = nn.relu(nn.Dense(512)(inputs["states"]))
+        x = nn.relu(nn.Dense(256)(x))
+        x = nn.Dense(self.num_actions)(x)
+        log_std = self.param("log_std", lambda _: jnp.zeros(self.num_actions))
+        return nn.tanh(x), log_std, {}
+
+class Critic(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device=None, clip_actions=False, **kwargs):
+        Model.__init__(self, observation_space, action_space, device, **kwargs)
+        DeterministicMixin.__init__(self, clip_actions)
+
+    def __hash__(self):  # for Isaac Sim 2022.2.1 or earlier
+        return id(self)
+
+    @nn.compact  # marks the given module method allowing inlined submodules
+    def __call__(self, inputs, role):
+        x = jnp.concatenate([inputs["states"], inputs["taken_actions"]], axis=-1)
+        x = nn.relu(nn.Dense(512)(x))
+        x = nn.relu(nn.Dense(256)(x))
+        x = nn.Dense(1)(x)
+        return x, {}
+
+
+# load and wrap the Omniverse Isaac Gym environment
+env = load_omniverse_isaacgym_env(task_name="Ant", num_envs=64)
+env = wrap_env(env)
+
+device = env.device
+
+
+# instantiate a memory as rollout buffer (any memory can be used for this)
+memory = RandomMemory(memory_size=15625, num_envs=env.num_envs, device=device)
+
+
+# instantiate the agent's models (function approximators).
+# SAC requires 5 models, visit its documentation for more details
+# https://skrl.readthedocs.io/en/latest/api/agents/sac.html#models
+models = {}
+models["policy"] = StochasticActor(env.observation_space, env.action_space, device)
+models["critic_1"] = Critic(env.observation_space, env.action_space, device)
+models["critic_2"] = Critic(env.observation_space, env.action_space, device)
+models["target_critic_1"] = Critic(env.observation_space, env.action_space, device)
+models["target_critic_2"] = Critic(env.observation_space, env.action_space, device)
+
+key = jax.random.PRNGKey(0)
+models["policy"].init_state_dict(key, {"states": env.observation_space.sample()}, "policy")
+models["critic_1"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "critic_1")
+models["critic_2"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "critic_2")
+models["target_critic_1"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "target_critic_1")
+models["target_critic_2"].init_state_dict(key, {"states": env.observation_space.sample(), "taken_actions": env.action_space.sample()}, "target_critic_2")
+
+
+# configure and instantiate the agent (visit its documentation to see all the options)
+# https://skrl.readthedocs.io/en/latest/api/agents/sac.html#configuration-and-hyperparameters
+cfg = SAC_DEFAULT_CONFIG.copy()
+cfg["gradient_steps"] = 1
+cfg["batch_size"] = 4096
+cfg["discount_factor"] = 0.99
+cfg["polyak"] = 0.005
+cfg["actor_learning_rate"] = 5e-4
+cfg["critic_learning_rate"] = 5e-4
+cfg["random_timesteps"] = 80
+cfg["learning_starts"] = 80
+cfg["grad_norm_clip"] = 0
+cfg["learn_entropy"] = True
+cfg["entropy_learning_rate"] = 5e-3
+cfg["initial_entropy_value"] = 1.0
+cfg["state_preprocessor"] = RunningStandardScaler
+cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 800
+cfg["experiment"]["checkpoint_interval"] = 8000
+cfg["experiment"]["directory"] = "runs/jax/Ant"
+
+agent = SAC(models=models,
+            memory=memory,
+            cfg=cfg,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=device)
+
+
+# configure and instantiate the RL trainer
+cfg_trainer = {"timesteps": 160000, "headless": True}
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
+
+# start training
+trainer.train()
---
+++ docs/source/examples/omniisaacgym/jax_crazyflie_ppo.py
@@ -0,0 +1,139 @@
+"""
+Notes for Isaac Sim 2022.2.1 or earlier (Python 3.7 environment):
+  * Python 3.7 is only supported up to jax<=0.3.25.
+    See: https://github.com/google/jax/blob/main/CHANGELOG.md#jaxlib-041-dec-13-2022.
+  * Builds for jaxlib<=0.3.25 are only available up to NVIDIA CUDA 11 and cuDNN 8.2 versions.
+    See: https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+    and search for `cuda11/jaxlib-0.3.25+cuda11.cudnn82-cp37-cp37m-manylinux2014_x86_64.whl`.
+  * The `jax.Device = jax.xla.Device` statement is required by skrl to support jax<0.4.3.
+  * Models require overloading the `__hash__` method to avoid "TypeError: Failed to hash Flax Module".
+"""
+
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+
+jax.Device = jax.xla.Device  # for Isaac Sim 2022.2.1 or earlier
+
+# import the skrl components to build the RL system
+from skrl import config
+from skrl.agents.jax.ppo import PPO, PPO_DEFAULT_CONFIG
+from skrl.envs.jax import load_omniverse_isaacgym_env, wrap_env
+from skrl.memories.jax import RandomMemory
+from skrl.models.jax import DeterministicMixin, GaussianMixin, Model
+from skrl.resources.preprocessors.jax import RunningStandardScaler
+from skrl.resources.schedulers.jax import KLAdaptiveRL
+from skrl.trainers.jax import SequentialTrainer
+from skrl.utils import set_seed
+
+
+config.jax.backend = "jax"  # or "numpy"
+
+
+# seed for reproducibility
+set_seed()  # e.g. `set_seed(42)` for fixed seed
+
+
+# define models (stochastic and deterministic models) using mixins
+class Policy(GaussianMixin, Model):
+    def __init__(self, observation_space, action_space, device=None, clip_actions=False,
+                 clip_log_std=True, min_log_std=-20, max_log_std=2, reduction="sum", **kwargs):
+        Model.__init__(self, observation_space, action_space, device, **kwargs)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
+
+    def __hash__(self):  # for Isaac Sim 2022.2.1 or earlier
+        return id(self)
+
+    @nn.compact  # marks the given module method allowing inlined submodules
+    def __call__(self, inputs, role):
+        x = nn.tanh(nn.Dense(256)(inputs["states"]))
+        x = nn.tanh(nn.Dense(256)(x))
+        x = nn.tanh(nn.Dense(128)(x))
+        x = nn.Dense(self.num_actions)(x)
+        log_std = self.param("log_std", lambda _: jnp.zeros(self.num_actions))
+        return x, log_std, {}
+
+class Value(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device=None, clip_actions=False, **kwargs):
+        Model.__init__(self, observation_space, action_space, device, **kwargs)
+        DeterministicMixin.__init__(self, clip_actions)
+
+    def __hash__(self):  # for Isaac Sim 2022.2.1 or earlier
+        return id(self)
+
+    @nn.compact  # marks the given module method allowing inlined submodules
+    def __call__(self, inputs, role):
+        x = nn.tanh(nn.Dense(256)(inputs["states"]))
+        x = nn.tanh(nn.Dense(256)(x))
+        x = nn.tanh(nn.Dense(128)(x))
+        x = nn.Dense(1)(x)
+        return x, {}
+
+
+# load and wrap the Omniverse Isaac Gym environment
+env = load_omniverse_isaacgym_env(task_name="Crazyflie")
+env = wrap_env(env)
+
+device = env.device
+
+
+# instantiate a memory as rollout buffer (any memory can be used for this)
+memory = RandomMemory(memory_size=16, num_envs=env.num_envs, device=device)
+
+
+# instantiate the agent's models (function approximators).
+# PPO requires 2 models, visit its documentation for more details
+# https://skrl.readthedocs.io/en/latest/api/agents/ppo.html#models
+models = {}
+models["policy"] = Policy(env.observation_space, env.action_space, device)
+models["value"] = Value(env.observation_space, env.action_space, device)
+
+key = jax.random.PRNGKey(0)
+models["policy"].init_state_dict(key, {"states": env.observation_space.sample()}, "policy")
+models["value"].init_state_dict(key, {"states": env.observation_space.sample()}, "value")
+
+
+# configure and instantiate the agent (visit its documentation to see all the options)
+# https://skrl.readthedocs.io/en/latest/api/agents/ppo.html#configuration-and-hyperparameters
+cfg = PPO_DEFAULT_CONFIG.copy()
+cfg["rollouts"] = 16  # memory_size
+cfg["learning_epochs"] = 8
+cfg["mini_batches"] = 4  # 16 * 4096 / 16384
+cfg["discount_factor"] = 0.99
+cfg["lambda"] = 0.95
+cfg["learning_rate"] = 1e-4
+cfg["learning_rate_scheduler"] = KLAdaptiveRL
+cfg["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.016}
+cfg["random_timesteps"] = 0
+cfg["learning_starts"] = 0
+cfg["grad_norm_clip"] = 1.0
+cfg["ratio_clip"] = 0.2
+cfg["value_clip"] = 0.2
+cfg["clip_predicted_values"] = True
+cfg["entropy_loss_scale"] = 0.0
+cfg["value_loss_scale"] = 1.0
+cfg["kl_threshold"] = 0
+cfg["rewards_shaper"] = lambda rewards, timestep, timesteps: rewards * 0.01
+cfg["state_preprocessor"] = RunningStandardScaler
+cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
+cfg["value_preprocessor"] = RunningStandardScaler
+cfg["value_preprocessor_kwargs"] = {"size": 1, "device": device}
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 80
+cfg["experiment"]["checkpoint_interval"] = 800
+cfg["experiment"]["directory"] = "runs/jax/Crazyflie"
+
+agent = PPO(models=models,
+            memory=memory,
+            cfg=cfg,
+            observation_space=env.observation_space,
+            action_space=env.action_space,
+            device=device)
+
+
+# configure and instantiate the RL trainer
+cfg_trainer = {"timesteps": 16000, "headless": True}
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
+
+# start training
+trainer.train()

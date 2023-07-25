935a05e5a96cdcec294fcec6d5c28a73732e9251
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-07-25 10:06:31 +0200

    Add Bi-DexHands example files

---
+++ docs/source/examples/bidexhands/jax_bidexhands_shadow_hand_over_ippo.py
@@ -0,0 +1,128 @@
+import isaacgym
+
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+
+# import the skrl components to build the RL system
+from skrl import config
+from skrl.envs.jax.loaders import load_bidexhands_env
+from skrl.envs.jax.wrappers import wrap_env
+from skrl.memories.jax import RandomMemory
+from skrl.models.jax import DeterministicMixin, GaussianMixin, Model
+from skrl.multi_agents.jax.ippo import IPPO, IPPO_DEFAULT_CONFIG
+from skrl.resources.preprocessors.jax import RunningStandardScaler
+from skrl.resources.schedulers.jax import KLAdaptiveRL
+from skrl.trainers.jax import SequentialTrainer
+from skrl.utils import set_seed
+
+
+config.jax.backend = "jax"  # or "numpy"
+
+
+# seed for reproducibility
+set_seed()  # e.g. `set_seed(42)` for fixed seed
+
+
+# define models (stochastic and deterministic models) using mixins
+class Policy(GaussianMixin, Model):
+    def __init__(self, observation_space, action_space, device=None, clip_actions=False,
+                 clip_log_std=True, min_log_std=-20, max_log_std=2, reduction="sum", **kwargs):
+        Model.__init__(self, observation_space, action_space, device, **kwargs)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
+
+    @nn.compact  # marks the given module method allowing inlined submodules
+    def __call__(self, inputs, role):
+        x = nn.elu(nn.Dense(512)(inputs["states"]))
+        x = nn.elu(nn.Dense(256)(x))
+        x = nn.elu(nn.Dense(128)(x))
+        x = nn.Dense(self.num_actions)(x)
+        log_std = self.param("log_std", lambda _: jnp.zeros(self.num_actions))
+        return x, log_std, {}
+
+class Value(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device=None, clip_actions=False, **kwargs):
+        Model.__init__(self, observation_space, action_space, device, **kwargs)
+        DeterministicMixin.__init__(self, clip_actions)
+
+    @nn.compact  # marks the given module method allowing inlined submodules
+    def __call__(self, inputs, role):
+        x = nn.elu(nn.Dense(512)(inputs["states"]))
+        x = nn.elu(nn.Dense(256)(x))
+        x = nn.elu(nn.Dense(128)(x))
+        x = nn.Dense(1)(x)
+        return x, {}
+
+
+# load and wrap the environment
+env = load_bidexhands_env(task_name="ShadowHandOver")
+env = wrap_env(env, wrapper="bidexhands")
+
+device = env.device
+
+
+# instantiate memories as rollout buffer (any memory can be used for this)
+memories = {}
+for agent_name in env.possible_agents:
+    memories[agent_name] = RandomMemory(memory_size=24, num_envs=env.num_envs, device=device)
+
+
+# instantiate the agent's models (function approximators).
+# IPPO requires 2 models, visit its documentation for more details
+# https://skrl.readthedocs.io/en/latest/api/multi_agents/ippo.html#models
+models = {}
+for agent_name in env.possible_agents:
+    models[agent_name] = {}
+    models[agent_name]["policy"] = Policy(env.observation_space(agent_name), env.action_space(agent_name), device)
+    models[agent_name]["value"] = Value(env.observation_space(agent_name), env.action_space(agent_name), device)
+
+# instantiate models' state dict
+for agent_name in env.possible_agents:
+    for role, model in models[agent_name].items():
+        model.init_state_dict(role)
+
+
+# configure and instantiate the agent (visit its documentation to see all the options)
+# https://skrl.readthedocs.io/en/latest/api/multi_agents/ippo.html#configuration-and-hyperparameters
+cfg = IPPO_DEFAULT_CONFIG.copy()
+cfg["rollouts"] = 24  # memory_size
+cfg["learning_epochs"] = 5
+cfg["mini_batches"] = 6  # 24 * 4096 / 16384
+cfg["discount_factor"] = 0.99
+cfg["lambda"] = 0.95
+cfg["learning_rate"] = 3e-4
+cfg["learning_rate_scheduler"] = KLAdaptiveRL
+cfg["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
+cfg["random_timesteps"] = 0
+cfg["learning_starts"] = 0
+cfg["grad_norm_clip"] = 1.0
+cfg["ratio_clip"] = 0.2
+cfg["value_clip"] = 0.2
+cfg["clip_predicted_values"] = True
+cfg["entropy_loss_scale"] = 0.001
+cfg["value_loss_scale"] = 1.0
+cfg["kl_threshold"] = 0
+cfg["state_preprocessor"] = RunningStandardScaler
+cfg["state_preprocessor_kwargs"] = {"size": next(iter(env.observation_spaces.values())), "device": device}
+cfg["value_preprocessor"] = RunningStandardScaler
+cfg["value_preprocessor_kwargs"] = {"size": 1, "device": device}
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 180
+cfg["experiment"]["checkpoint_interval"] = 1800
+cfg["experiment"]["directory"] = "runs/jax/ShadowHandOver"
+
+agent = IPPO(possible_agents=env.possible_agents,
+             models=models,
+             memories=memories,
+             cfg=cfg,
+             observation_spaces=env.observation_spaces,
+             action_spaces=env.action_spaces,
+             device=device)
+
+
+# configure and instantiate the RL trainer
+cfg_trainer = {"timesteps": 36000, "headless": True}
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
+
+# start training
+trainer.train()
---
+++ docs/source/examples/bidexhands/torch_bidexhands_shadow_hand_over_mappo.py
@@ -0,0 +1,129 @@
+import isaacgym
+
+import torch
+import torch.nn as nn
+
+# import the skrl components to build the RL system
+from skrl.envs.torch.loaders import load_bidexhands_env
+from skrl.envs.torch.wrappers import wrap_env
+from skrl.memories.torch import RandomMemory
+from skrl.models.torch import DeterministicMixin, GaussianMixin, Model
+from skrl.multi_agents.torch.mappo import MAPPO, MAPPO_DEFAULT_CONFIG
+from skrl.resources.preprocessors.torch import RunningStandardScaler
+from skrl.resources.schedulers.torch import KLAdaptiveRL
+from skrl.trainers.torch import SequentialTrainer
+from skrl.utils import set_seed
+
+
+# seed for reproducibility
+set_seed()  # e.g. `set_seed(42)` for fixed seed
+
+
+# define models (stochastic and deterministic models) using mixins
+class Policy(GaussianMixin, Model):
+    def __init__(self, observation_space, action_space, device, clip_actions=False,
+                 clip_log_std=True, min_log_std=-20, max_log_std=2, reduction="sum"):
+        Model.__init__(self, observation_space, action_space, device)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
+
+        self.net = nn.Sequential(nn.Linear(self.num_observations, 512),
+                                 nn.ELU(),
+                                 nn.Linear(512, 256),
+                                 nn.ELU(),
+                                 nn.Linear(256, 128),
+                                 nn.ELU(),
+                                 nn.Linear(128, self.num_actions))
+        self.log_std_parameter = nn.Parameter(torch.zeros(self.num_actions))
+
+    def compute(self, inputs, role):
+        return self.net(inputs["states"]), self.log_std_parameter, {}
+
+class Value(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device, clip_actions=False):
+        Model.__init__(self, observation_space, action_space, device)
+        DeterministicMixin.__init__(self, clip_actions)
+
+        self.net = nn.Sequential(nn.Linear(self.num_observations, 512),
+                                 nn.ELU(),
+                                 nn.Linear(512, 256),
+                                 nn.ELU(),
+                                 nn.Linear(256, 128),
+                                 nn.ELU(),
+                                 nn.Linear(128, 1))
+
+    def compute(self, inputs, role):
+        return self.net(inputs["states"]), {}
+
+
+# load and wrap the environment
+env = load_bidexhands_env(task_name="ShadowHandOver")
+env = wrap_env(env, wrapper="bidexhands")
+
+device = env.device
+
+
+# instantiate memories as rollout buffer (any memory can be used for this)
+memories = {}
+for agent_name in env.possible_agents:
+    memories[agent_name] = RandomMemory(memory_size=24, num_envs=env.num_envs, device=device)
+
+
+# instantiate the agent's models (function approximators).
+# MAPPO requires 2 models, visit its documentation for more details
+# https://skrl.readthedocs.io/en/latest/api/multi_agents/mappo.html#models
+models = {}
+for agent_name in env.possible_agents:
+    models[agent_name] = {}
+    models[agent_name]["policy"] = Policy(env.observation_space(agent_name), env.action_space(agent_name), device)
+    models[agent_name]["value"] = Value(env.shared_observation_space(agent_name), env.action_space(agent_name), device)
+
+
+# configure and instantiate the agent (visit its documentation to see all the options)
+# https://skrl.readthedocs.io/en/latest/api/multi_agents/mappo.html#configuration-and-hyperparameters
+cfg = MAPPO_DEFAULT_CONFIG.copy()
+cfg["rollouts"] = 24  # memory_size
+cfg["learning_epochs"] = 5
+cfg["mini_batches"] = 6  # 24 * 4096 / 16384
+cfg["discount_factor"] = 0.99
+cfg["lambda"] = 0.95
+cfg["learning_rate"] = 3e-4
+cfg["learning_rate_scheduler"] = KLAdaptiveRL
+cfg["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
+cfg["random_timesteps"] = 0
+cfg["learning_starts"] = 0
+cfg["grad_norm_clip"] = 1.0
+cfg["ratio_clip"] = 0.2
+cfg["value_clip"] = 0.2
+cfg["clip_predicted_values"] = True
+cfg["entropy_loss_scale"] = 0.001
+cfg["value_loss_scale"] = 1.0
+cfg["kl_threshold"] = 0
+cfg["state_preprocessor"] = RunningStandardScaler
+cfg["state_preprocessor_kwargs"] = {"size": next(iter(env.observation_spaces.values())), "device": device}
+cfg["shared_state_preprocessor"] = RunningStandardScaler
+cfg["shared_state_preprocessor_kwargs"] = {
+    "size": next(iter(env.shared_observation_spaces.values())), "device": device
+}
+cfg["value_preprocessor"] = RunningStandardScaler
+cfg["value_preprocessor_kwargs"] = {"size": 1, "device": device}
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 180
+cfg["experiment"]["checkpoint_interval"] = 1800
+cfg["experiment"]["directory"] = "runs/torch/ShadowHandOver"
+
+agent = MAPPO(possible_agents=env.possible_agents,
+              models=models,
+              memories=memories,
+              cfg=cfg,
+              observation_spaces=env.observation_spaces,
+              action_spaces=env.action_spaces,
+              device=device,
+              shared_observation_spaces=env.shared_observation_spaces)
+
+
+# configure and instantiate the RL trainer
+cfg_trainer = {"timesteps": 36000, "headless": True}
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
+
+# start training
+trainer.train()

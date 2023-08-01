90c6ff0a5a4e920f4e327f55d16b326b95a872bc
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-08-01 23:04:30 +0200

    Update Isaac Sim examples

--- docs/source/examples/isaacsim/torch_isaacsim_jetbot_ppo.py
+++ docs/source/examples/isaacsim/torch_isaacsim_jetbot_ppo.py
@@ -4,22 +4,25 @@ from env import JetBotEnv
 import torch
 import torch.nn as nn
 
-# Import the skrl components to build the RL system
-from skrl.models.torch import Model, GaussianMixin, DeterministicMixin
-from skrl.memories.torch import RandomMemory
+# import the skrl components to build the RL system
 from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
+from skrl.envs.wrappers.torch import wrap_env
+from skrl.memories.torch import RandomMemory
+from skrl.models.torch import DeterministicMixin, GaussianMixin, Model
 from skrl.trainers.torch import SequentialTrainer
-from skrl.envs.torch import wrap_env
+from skrl.utils import set_seed
 
 
-# Define the models (stochastic and deterministic models) for the agent using mixins.
-# - Policy: takes as input the environment's observation/state and returns an action
-# - Value: takes the state as input and provides a value to guide the policy
+# seed for reproducibility
+set_seed()  # e.g. `set_seed(42)` for fixed seed
+
+
+# define models (stochastic and deterministic models) using mixins
 class Policy(GaussianMixin, Model):
     def __init__(self, observation_space, action_space, device, clip_actions=False,
-                 clip_log_std=True, min_log_std=-20, max_log_std=2):
+                 clip_log_std=True, min_log_std=-20, max_log_std=2, reduction="sum"):
         Model.__init__(self, observation_space, action_space, device)
-        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
 
         self.net = nn.Sequential(nn.Conv2d(3, 32, kernel_size=8, stride=4),
                                  nn.ReLU(),
@@ -73,68 +76,62 @@ class Value(DeterministicMixin, Model):
         return self.net(inputs["states"].view(-1, *self.observation_space.shape).permute(0, 3, 1, 2)), {}
 
 
-# Load and wrap the JetBot environment (a subclass of Gym)
+# load and wrap the environment
 env = JetBotEnv(headless=True)
 env = wrap_env(env)
 
 device = env.device
 
 
-# Instantiate a RandomMemory as rollout buffer (any memory can be used for this)
+# instantiate a memory as rollout buffer (any memory can be used for this)
 memory = RandomMemory(memory_size=10000, num_envs=env.num_envs, device=device)
 
 
-# Instantiate the agent's models (function approximators).
+# instantiate the agent's models (function approximators).
 # PPO requires 2 models, visit its documentation for more details
-# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#spaces-and-models
-models_ppo = {}
-models_ppo["policy"] = Policy(env.observation_space, env.action_space, device)
-models_ppo["value"] = Value(env.observation_space, env.action_space, device)
+# https://skrl.readthedocs.io/en/latest/api/agents/ppo.html#models
+models = {}
+models["policy"] = Policy(env.observation_space, env.action_space, device, clip_actions=True)
+models["value"] = Value(env.observation_space, env.action_space, device)
 
-# Initialize the models' parameters (weights and biases) using a Gaussian distribution
-for model in models_ppo.values():
+# initialize models' parameters (weights and biases)
+for model in models.values():
     model.init_parameters(method_name="normal_", mean=0.0, std=0.1)
 
 
-# Configure and instantiate the agent.
-# Only modify some of the default configuration, visit its documentation to see all the options
-# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#configuration-and-hyperparameters
-cfg_ppo = PPO_DEFAULT_CONFIG.copy()
-cfg_ppo["rollouts"] = 10000
-cfg_ppo["learning_epochs"] = 10
-cfg_ppo["mini_batches"] = 10
-cfg_ppo["discount_factor"] = 0.9995
-cfg_ppo["lambda"] = 0.95
-cfg_ppo["policy_learning_rate"] = 0.00025
-cfg_ppo["value_learning_rate"] = 0.00025
-cfg_ppo["random_timesteps"] = 0
-cfg_ppo["learning_starts"] = 0
-cfg_ppo["grad_norm_clip"] = 10
-cfg_ppo["ratio_clip"] = 0.2
-cfg_ppo["value_clip"] = 0.2
-cfg_ppo["clip_predicted_values"] = False
-cfg_ppo["entropy_loss_scale"] = 0.0
-cfg_ppo["value_loss_scale"] = 0.5
-cfg_ppo["kl_threshold"] = 0
-# logging to TensorBoard and write checkpoints each 10000 timesteps
-cfg_ppo["experiment"]["write_interval"] = 10000
-cfg_ppo["experiment"]["checkpoint_interval"] = 10000
+# configure and instantiate the agent (visit its documentation to see all the options)
+# https://skrl.readthedocs.io/en/latest/api/agents/ppo.html#configuration-and-hyperparameters
+cfg = PPO_DEFAULT_CONFIG.copy()
+cfg["rollouts"] = 10000
+cfg["learning_epochs"] = 10
+cfg["mini_batches"] = 10
+cfg["discount_factor"] = 0.9995
+cfg["lambda"] = 0.95
+cfg["policy_learning_rate"] = 2.5e-4
+cfg["value_learning_rate"] = 2.5e-4
+cfg["grad_norm_clip"] = 10
+cfg["ratio_clip"] = 0.2
+cfg["value_clip"] = 0.2
+cfg["clip_predicted_values"] = False
+cfg["entropy_loss_scale"] = 0.0
+cfg["value_loss_scale"] = 0.5
+cfg["kl_threshold"] = 0
+# logging to TensorBoard and write checkpoints (in timesteps)
+cfg["experiment"]["write_interval"] = 10000
+cfg["experiment"]["checkpoint_interval"] = 10000
+cfg["experiment"]["directory"] = "runs/torch/JetBotEnv"
 
-agent = PPO(models=models_ppo,
+agent = PPO(models=models,
             memory=memory,
-            cfg=cfg_ppo,
+            cfg=cfg,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=device)
 
 
-# Configure and instanciate the RL trainer
+# configure and instantiate the RL trainer
 cfg_trainer = {"timesteps": 500000, "headless": True}
 trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 
 # start training
 trainer.train()
-
-
-# close the environment
-env.close()

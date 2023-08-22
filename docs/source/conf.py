c73421b30c6147219b8de2e48365fc2b6e64cdbe
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-08-22 22:36:09 +0200

    Increase MINOR version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ author = "Toni-SM"
 if skrl.__version__ != "unknown":
     release = version = skrl.__version__
 else:
-    release = version = "1.0.0"
+    release = version = "1.1.0"
 
 master_doc = "index"
 

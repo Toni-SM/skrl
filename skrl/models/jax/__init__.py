5222dbb12c5c7f1c37d2196decdbf49dadae5a38
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-09-30 20:46:00 +0200

    Add multi-categorical model mixin in jax

--- skrl/models/jax/__init__.py
+++ skrl/models/jax/__init__.py
@@ -3,3 +3,4 @@ from skrl.models.jax.base import Model  # isort:skip
 from skrl.models.jax.categorical import CategoricalMixin
 from skrl.models.jax.deterministic import DeterministicMixin
 from skrl.models.jax.gaussian import GaussianMixin
+from skrl.models.jax.multicategorical import MultiCategoricalMixin

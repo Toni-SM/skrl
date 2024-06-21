159d39e343739e3d7628f8c1d9f8c15efaa19948
Antonio Serrano Muñoz <toni.semu@gmail.com>
2024-06-21 13:38:59 -0400

    Rename Orbit environment loader to Isaac Lab

--- skrl/envs/loaders/torch/__init__.py
+++ skrl/envs/loaders/torch/__init__.py
@@ -4,5 +4,5 @@ from skrl.envs.loaders.torch.isaacgym_envs import (
     load_isaacgym_env_preview3,
     load_isaacgym_env_preview4
 )
-from skrl.envs.loaders.torch.isaaclab_envs import load_isaac_orbit_env
+from skrl.envs.loaders.torch.isaaclab_envs import load_isaaclab_env
 from skrl.envs.loaders.torch.omniverse_isaacgym_envs import load_omniverse_isaacgym_env

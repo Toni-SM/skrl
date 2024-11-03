b65872cbfc186b401c285be96397bfd3046db7ec
Toni-SM <toni.semu@gmail.com>
2024-11-03 10:00:14 -0500

    Apply format to runner.py in torch

--- skrl/utils/runner/torch/runner.py
+++ skrl/utils/runner/torch/runner.py
@@ -14,7 +14,7 @@ from skrl.resources.preprocessors.torch import RunningStandardScaler  # noqa
 from skrl.resources.schedulers.torch import KLAdaptiveLR  # noqa
 from skrl.trainers.torch import SequentialTrainer, Trainer
 from skrl.utils import set_seed
-from skrl.utils.model_instantiators.torch import deterministic_model, gaussian_model, shared_model, categorical_model
+from skrl.utils.model_instantiators.torch import categorical_model, deterministic_model, gaussian_model, shared_model
 
 
 class Runner:
@@ -35,9 +35,9 @@ class Runner:
         self._class_mapping = {
             # model
             "gaussianmixin": gaussian_model,
+            "categoricalmixin": categorical_model,
             "deterministicmixin": deterministic_model,
             "shared": shared_model,
-            "categoricalmixin": categorical_model,
             # memory
             "randommemory": RandomMemory,
             # agent

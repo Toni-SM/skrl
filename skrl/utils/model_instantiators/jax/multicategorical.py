a2c64dd6089753a622317b904f52a50bb21bf3ab
Antonio Serrano Muñoz <toni.semu@gmail.com>
2025-03-02 15:35:33 -0500

    Update model instantiators in jax

--- skrl/utils/model_instantiators/jax/multicategorical.py
+++ skrl/utils/model_instantiators/jax/multicategorical.py
@@ -7,52 +7,43 @@ import flax.linen as nn  # noqa
 import jax
 import jax.numpy as jnp  # noqa
 
-from skrl.models.jax import GaussianMixin  # noqa
+from skrl.models.jax import MultiCategoricalMixin  # noqa
 from skrl.models.jax import Model
 from skrl.utils.model_instantiators.jax.common import generate_containers
 from skrl.utils.spaces.jax import unflatten_tensorized_space  # noqa
 
 
-def gaussian_model(
+def multicategorical_model(
     *,
     observation_space: Optional[gymnasium.Space] = None,
     state_space: Optional[gymnasium.Space] = None,
     action_space: Optional[gymnasium.Space] = None,
     device: Optional[Union[str, jax.Device]] = None,
-    clip_actions: bool = False,
-    clip_log_std: bool = True,
-    min_log_std: float = -20,
-    max_log_std: float = 2,
+    unnormalized_log_prob: bool = True,
     reduction: str = "sum",
-    initial_log_std: float = 0,
-    fixed_log_std: bool = False,
     network: Sequence[Mapping[str, Any]] = [],
     output: Union[str, Sequence[str]] = "",
     return_source: bool = False,
 ) -> Union[Model, str]:
-    """Instantiate a :class:`~skrl.models.jax.gaussian.GaussianMixin`-based model.
+    """Instantiate a :class:`~skrl.models.jax.multicategorical.MultiCategoricalMixin`-based model.
 
     :param observation_space: Observation space. The ``num_observations`` property will contain the size of the space.
     :param state_space: State space. The ``num_states`` property will contain the size of the space.
     :param action_space: Action space. The ``num_actions`` property will contain the size of the space.
     :param device: Data allocation and computation device. If not specified, the default device will be used.
-    :param clip_actions: Flag to indicate whether the actions should be clipped to the action space.
-    :param clip_log_std: Flag to indicate whether the log standard deviations should be clipped.
-    :param min_log_std: Minimum value of the log standard deviation if ``clip_log_std`` is True.
-    :param max_log_std: Maximum value of the log standard deviation if ``clip_log_std`` is True.
+    :param unnormalized_log_prob: Flag to indicate how to the model's output will be interpreted.
+        If True, the model's output is interpreted as unnormalized log probabilities (it can be any real number),
+        otherwise as normalized probabilities (the output must be non-negative, finite and have a non-zero sum).
     :param reduction: Reduction method for returning the log probability density function.
         Supported values are ``"mean"``, ``"sum"``, ``"prod"`` and ``"none"``.
         If ``"none"``, the log probability density function is returned as a tensor of shape
         ``(num_samples, num_actions)`` instead of ``(num_samples, 1)``.
-    :param initial_log_std: Initial value for the log standard deviation.
-    :param fixed_log_std: Whether the log standard deviation parameter should be fixed.
-        Fixed parameters have the gradient computation deactivated.
     :param network: Network definition.
     :param output: Output expression.
     :param return_source: Whether to return the source string containing the model class used to
         instantiate the model rather than the model instance.
 
-    :return: Gaussian model instance or definition source (if ``return_source`` is True).
+    :return: MultiCategorical model instance or definition source (if ``return_source`` is True).
     """
     # parse model definition
     containers, output = generate_containers(network, output, embed_output=True, indent=1)
@@ -75,24 +66,15 @@ def gaussian_model(
     # build substitutions and indent content
     networks = textwrap.indent("\n".join(networks), prefix=" " * 8)[8:]
     forward = textwrap.indent("\n".join(forward), prefix=" " * 8)[8:]
-    if fixed_log_std:
-        log_std_parameter = f'jnp.full(shape={output["size"]}, fill_value={initial_log_std})'
-    else:
-        log_std_parameter = (
-            f'self.param("log_std_parameter", lambda _: jnp.full(shape={output["size"]}, fill_value={initial_log_std}))'
-        )
 
-    template = f"""class GaussianModel(GaussianMixin, Model):
+    template = f"""class MultiCategoricalModel(MultiCategoricalMixin, Model):
     def __init__(
         self,
         observation_space,
         state_space,
         action_space,
         device=None,
-        clip_actions=False,
-        clip_log_std=True,
-        min_log_std=-20,
-        max_log_std=2,
+        unnormalized_log_prob=True,
         reduction="sum",
         role="",
         **kwargs,
@@ -105,26 +87,17 @@ def gaussian_model(
             device=device,
             **kwargs,
         )
-        GaussianMixin.__init__(
-            self,
-            clip_actions=clip_actions,
-            clip_log_std=clip_log_std,
-            min_log_std=min_log_std,
-            max_log_std=max_log_std,
-            reduction=reduction,
-            role=role,
-        )
+        MultiCategoricalMixin.__init__(self, unnormalized_log_prob=unnormalized_log_prob, reduction=reduction, role=role)
 
     def setup(self):
         {networks}
-        self.log_std_parameter = {log_std_parameter}
 
     def __call__(self, inputs, role=""):
         observations = unflatten_tensorized_space(self.observation_space, inputs.get("observations"))
         states = unflatten_tensorized_space(self.state_space, inputs.get("states"))
         taken_actions = unflatten_tensorized_space(self.action_space, inputs.get("taken_actions"))
         {forward}
-        return output, {{"log_std": self.log_std_parameter}}
+        return output, {{}}
     """
     # return source
     if return_source:
@@ -133,14 +106,11 @@ def gaussian_model(
     # instantiate model
     _locals = {}
     exec(template, globals(), _locals)
-    return _locals["GaussianModel"](
+    return _locals["MultiCategoricalModel"](
         observation_space=observation_space,
         state_space=state_space,
         action_space=action_space,
         device=device,
-        clip_actions=clip_actions,
-        clip_log_std=clip_log_std,
-        min_log_std=min_log_std,
-        max_log_std=max_log_std,
+        unnormalized_log_prob=unnormalized_log_prob,
         reduction=reduction,
     )

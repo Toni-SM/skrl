b30342e9c62d7e3ec15ee12a1d671b485f715c09
Antonio Serrano Munoz <aserranomuno@nvidia.com>
2025-06-17 16:19:13 -0400

    Add initial wrapper implementation

---
+++ skrl/envs/wrappers/warp/__init__.py
@@ -0,0 +1,174 @@
+from typing import Any, Union
+
+import re
+
+from skrl import logger
+from skrl.envs.wrappers.warp.base import MultiAgentEnvWrapper, Wrapper
+from skrl.envs.wrappers.warp.gymnasium_envs import GymnasiumWrapper
+
+
+__all__ = ["wrap_env", "Wrapper", "MultiAgentEnvWrapper"]
+
+
+def wrap_env(env: Any, wrapper: str = "auto", verbose: bool = True) -> Union[Wrapper, MultiAgentEnvWrapper]:
+    """Wrap an environment to use a common interface
+
+    Example::
+
+        >>> from skrl.envs.wrappers.warp import wrap_env
+        >>>
+        >>> # assuming that there is an environment called "env"
+        >>> env = wrap_env(env)
+
+    :param env: The environment to be wrapped
+    :type env: Any
+    :param wrapper: The type of wrapper to use (default: ``"auto"``).
+                    If ``"auto"``, the wrapper will be automatically selected based on the environment class.
+                    The supported wrappers are described in the following table:
+
+                    .. list-table:: Single-agent environments |br|
+                        :header-rows: 1
+
+                        * - Environment
+                          - Wrapper tag
+                        * - OpenAI Gym
+                          - ``"gym"``
+                        * - Gymnasium
+                          - ``"gymnasium"``
+                        * - Brax
+                          - ``"brax"``
+                        * - Isaac Lab
+                          - ``"isaaclab"`` (``"isaaclab-single-agent"``)
+                        * - Isaac Gym preview 2
+                          - ``"isaacgym-preview2"``
+                        * - Isaac Gym preview 3
+                          - ``"isaacgym-preview3"``
+                        * - Isaac Gym preview 4
+                          - ``"isaacgym-preview4"``
+                        * - Omniverse Isaac Gym
+                          - ``"omniverse-isaacgym"``
+
+                    .. list-table:: Multi-agent environments |br|
+                        :header-rows: 1
+
+                        * - Environment
+                          - Wrapper tag
+                        * - Petting Zoo
+                          - ``"pettingzoo"``
+                        * - Isaac Lab
+                          - ``"isaaclab"`` (``"isaaclab-multi-agent"``)
+                        * - Bi-DexHands
+                          - ``"bidexhands"``
+    :type wrapper: str, optional
+    :param verbose: Whether to print the wrapper type (default: ``True``)
+    :type verbose: bool, optional
+
+    :raises ValueError: Unknown wrapper type
+
+    :return: Wrapped environment
+    :rtype: Wrapper or MultiAgentEnvWrapper
+    """
+
+    def _get_wrapper_name(env, verbose):
+        def _in(values, container):
+            if type(values) == str:
+                values = [values]
+            for item in container:
+                for value in values:
+                    if value in item or re.match(value, item):
+                        return True
+            return False
+
+        base_classes = [str(base).replace("<class '", "").replace("'>", "") for base in env.__class__.__bases__]
+        try:
+            base_classes += [
+                str(base).replace("<class '", "").replace("'>", "") for base in env.unwrapped.__class__.__bases__
+            ]
+        except:
+            pass
+        base_classes = sorted(list(set(base_classes)))
+        if verbose:
+            logger.info(f"Environment wrapper: 'auto' (class: {', '.join(base_classes)})")
+
+        if _in(["omni.isaac.lab.*", "isaaclab.*"], base_classes):
+            return "isaaclab-*"
+        elif _in("omni.isaac.gym..*", base_classes):
+            return "omniverse-isaacgym"
+        elif _in(["isaacgymenvs..*", "tasks..*.VecTask"], base_classes):
+            return "isaacgym-preview4"  # preview 4 is the same as 3
+        elif _in("rlgpu.tasks..*.VecTask", base_classes):
+            return "isaacgym-preview2"
+        elif _in("brax.envs..*", base_classes):
+            return "brax"
+        elif _in("robosuite.environments.", base_classes):
+            return "robosuite"
+        elif _in("dm_env..*", base_classes):
+            return "dm"
+        elif _in("pettingzoo.utils.env", base_classes) or _in("pettingzoo.utils.wrappers", base_classes):
+            return "pettingzoo"
+        elif _in("gymnasium..*", base_classes):
+            return "gymnasium"
+        elif _in("gym..*", base_classes):
+            return "gym"
+        return base_classes
+
+    if wrapper == "auto":
+        wrapper = _get_wrapper_name(env, verbose)
+
+    if wrapper == "gym":
+        if verbose:
+            logger.info("Environment wrapper: Gym")
+        return GymWrapper(env)
+    elif wrapper == "gymnasium":
+        if verbose:
+            logger.info("Environment wrapper: gymnasium")
+        return GymnasiumWrapper(env)
+    elif wrapper == "pettingzoo":
+        if verbose:
+            logger.info("Environment wrapper: Petting Zoo")
+        return PettingZooWrapper(env)
+    elif wrapper == "bidexhands":
+        if verbose:
+            logger.info("Environment wrapper: Bi-DexHands")
+        return BiDexHandsWrapper(env)
+    elif wrapper == "brax":
+        if verbose:
+            logger.info("Environment wrapper: Brax")
+        return BraxWrapper(env)
+    elif wrapper == "isaacgym-preview2":
+        if verbose:
+            logger.info("Environment wrapper: Isaac Gym (preview 2)")
+        return IsaacGymPreview2Wrapper(env)
+    elif wrapper == "isaacgym-preview3":
+        if verbose:
+            logger.info("Environment wrapper: Isaac Gym (preview 3)")
+        return IsaacGymPreview3Wrapper(env)
+    elif wrapper == "isaacgym-preview4":
+        if verbose:
+            logger.info("Environment wrapper: Isaac Gym (preview 4)")
+        return IsaacGymPreview3Wrapper(env)  # preview 4 is the same as 3
+    elif wrapper == "omniverse-isaacgym":
+        if verbose:
+            logger.info("Environment wrapper: Omniverse Isaac Gym")
+        return OmniverseIsaacGymWrapper(env)
+    elif type(wrapper) is str and wrapper.startswith("isaaclab"):
+        # use specified wrapper
+        if wrapper == "isaaclab-single-agent":
+            env_type = "single-agent"
+            env_wrapper = IsaacLabWrapper
+        elif wrapper == "isaaclab-multi-agent":
+            env_type = "multi-agent"
+            env_wrapper = IsaacLabMultiAgentWrapper
+        # detect the wrapper
+        else:
+            env_type = "single-agent"
+            env_wrapper = IsaacLabWrapper
+            if hasattr(env.unwrapped, "possible_agents"):
+                env_type = "multi-agent"
+                env_wrapper = IsaacLabMultiAgentWrapper
+        # wrap the environment
+        if verbose:
+            logger.info(f"Environment wrapper: Isaac Lab ({env_type})")
+        return env_wrapper(env)
+    else:
+        raise ValueError(f"Unknown wrapper type: {wrapper}")
---
+++ skrl/envs/wrappers/warp/base.py
@@ -0,0 +1,149 @@
+from typing import Any, Mapping, Sequence, Tuple, Union
+
+import gymnasium
+
+import warp as wp
+
+from skrl import config
+
+
+class Wrapper(object):
+    def __init__(self, env: Any) -> None:
+        """Base wrapper class for RL environments
+
+        :param env: The environment to wrap
+        :type env: Any supported RL environment
+        """
+        self._env = env
+        try:
+            self._unwrapped = self._env.unwrapped
+        except:
+            self._unwrapped = env
+
+        # device
+        if hasattr(self._unwrapped, "device"):
+            self._device = config.warp.parse_device(self._unwrapped.device)
+        else:
+            self._device = config.warp.parse_device(None)
+
+    def __getattr__(self, key: str) -> Any:
+        """Get an attribute from the wrapped environment
+
+        :param key: The attribute name
+        :type key: str
+
+        :raises AttributeError: If the attribute does not exist
+
+        :return: The attribute value
+        :rtype: Any
+        """
+        if hasattr(self._env, key):
+            return getattr(self._env, key)
+        if hasattr(self._unwrapped, key):
+            return getattr(self._unwrapped, key)
+        raise AttributeError(
+            f"Wrapped environment ({self._unwrapped.__class__.__name__}) does not have attribute '{key}'"
+        )
+
+    def reset(self) -> Tuple[wp.array, Any]:
+        """Reset the environment
+
+        :raises NotImplementedError: Not implemented
+
+        :return: Observation, info
+        :rtype: wp.array and any other info
+        """
+        raise NotImplementedError
+
+    def step(self, actions: wp.array) -> Tuple[
+        wp.array,
+        wp.array,
+        wp.array,
+        wp.array,
+        Any,
+    ]:
+        """Perform a step in the environment
+
+        :param actions: The actions to perform
+        :type actions: wp.array
+
+        :raises NotImplementedError: Not implemented
+
+        :return: Observation, reward, terminated, truncated, info
+        :rtype: tuple of wp.array and any other info
+        """
+        raise NotImplementedError
+
+    def state(self) -> wp.array:
+        """Get the environment state
+
+        :raises NotImplementedError: Not implemented
+
+        :return: State
+        :rtype: wp.array
+        """
+        raise NotImplementedError
+
+    def render(self, *args, **kwargs) -> Any:
+        """Render the environment
+
+        :raises NotImplementedError: Not implemented
+
+        :return: Any value from the wrapped environment
+        :rtype: any
+        """
+        raise NotImplementedError
+
+    def close(self) -> None:
+        """Close the environment
+
+        :raises NotImplementedError: Not implemented
+        """
+        raise NotImplementedError
+
+    @property
+    def device(self) -> wp.context.Device:
+        """The device used by the environment
+
+        If the wrapped environment does not have the ``device`` property, the value of this property
+        will be ``"cuda"`` or ``"cpu"`` depending on the device availability
+        """
+        return self._device
+
+    @property
+    def num_envs(self) -> int:
+        """Number of environments
+
+        If the wrapped environment does not have the ``num_envs`` property, it will be set to 1
+        """
+        return self._unwrapped.num_envs if hasattr(self._unwrapped, "num_envs") else 1
+
+    @property
+    def num_agents(self) -> int:
+        """Number of agents
+
+        If the wrapped environment does not have the ``num_agents`` property, it will be set to 1
+        """
+        return self._unwrapped.num_agents if hasattr(self._unwrapped, "num_agents") else 1
+
+    @property
+    def state_space(self) -> Union[gymnasium.Space, None]:
+        """State space
+
+        If the wrapped environment does not have the ``state_space`` property, ``None`` will be returned
+        """
+        return self._unwrapped.state_space if hasattr(self._unwrapped, "state_space") else None
+
+    @property
+    def observation_space(self) -> gymnasium.Space:
+        """Observation space"""
+        return self._unwrapped.observation_space
+
+    @property
+    def action_space(self) -> gymnasium.Space:
+        """Action space"""
+        return self._unwrapped.action_space
+
+
+class MultiAgentEnvWrapper(object):
+    pass

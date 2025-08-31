c78fb5a0392b3f43209e8a8c8f4b45aef2003b17
Antonio Serrano Muñoz <toni.semu@gmail.com>
2025-08-30 22:51:50 -0400

    Update on-policy examples

--- examples/gymnasium/torch_gymnasium_pendulum_ppo.py
+++ examples/gymnasium/torch_gymnasium_pendulum_ppo.py
@@ -1,21 +1,34 @@
+import argparse
+import os
 import gymnasium as gym
 
 import torch
 import torch.nn as nn
 
 # import the skrl components to build the RL system
+from skrl import logger
 from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
 from skrl.envs.wrappers.torch import wrap_env
 from skrl.memories.torch import RandomMemory
 from skrl.models.torch import DeterministicMixin, GaussianMixin, Model
 from skrl.resources.preprocessors.torch import RunningStandardScaler
-from skrl.resources.schedulers.torch import KLAdaptiveRL
+from skrl.resources.schedulers.torch import KLAdaptiveLR
 from skrl.trainers.torch import SequentialTrainer
 from skrl.utils import set_seed
 
 
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--num_envs", type=int, default=1, help="Number of environments")
+parser.add_argument("--headless", action="store_true", help="Run in headless mode (no rendering)")
+parser.add_argument("--seed", type=int, default=None, help="Random seed")
+parser.add_argument("--checkpoint", type=str, default=None, help="Load checkpoint from path")
+parser.add_argument("--eval", action="store_true", help="Run in evaluation mode (logging/checkpointing disabled)")
+args, _ = parser.parse_known_args()
+
+
 # seed for reproducibility
-set_seed()  # e.g. `set_seed(42)` for fixed seed
+set_seed(args.seed)  # e.g. `set_seed(42)` for fixed seed
 
 
 # define models (stochastic and deterministic models) using mixins
@@ -23,6 +36,7 @@ class Policy(GaussianMixin, Model):
     def __init__(
         self,
         observation_space,
+        state_space,
         action_space,
         device,
         clip_actions=False,
@@ -31,8 +45,17 @@ class Policy(GaussianMixin, Model):
         max_log_std=2,
         reduction="sum",
     ):
-        Model.__init__(self, observation_space, action_space, device)
-        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
+        Model.__init__(
+            self, observation_space=observation_space, state_space=state_space, action_space=action_space, device=device
+        )
+        GaussianMixin.__init__(
+            self,
+            clip_actions=clip_actions,
+            clip_log_std=clip_log_std,
+            min_log_std=min_log_std,
+            max_log_std=max_log_std,
+            reduction=reduction,
+        )
 
         self.net = nn.Sequential(
             nn.Linear(self.num_observations, 64),
@@ -45,32 +68,39 @@ class Policy(GaussianMixin, Model):
 
     def compute(self, inputs, role):
         # Pendulum-v1 action_space is -2 to 2
-        return 2 * torch.tanh(self.net(inputs["states"])), self.log_std_parameter, {}
+        return 2 * torch.tanh(self.net(inputs["observations"])), {"log_std": self.log_std_parameter}
 
 
 class Value(DeterministicMixin, Model):
-    def __init__(self, observation_space, action_space, device, clip_actions=False):
-        Model.__init__(self, observation_space, action_space, device)
-        DeterministicMixin.__init__(self, clip_actions)
+    def __init__(self, observation_space, state_space, action_space, device):
+        Model.__init__(
+            self, observation_space=observation_space, state_space=state_space, action_space=action_space, device=device
+        )
+        DeterministicMixin.__init__(self)
 
         self.net = nn.Sequential(
-            nn.Linear(self.num_observations, 64), nn.ReLU(), nn.Linear(64, 64), nn.ReLU(), nn.Linear(64, 1)
+            nn.Linear(self.num_observations, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1),
         )
 
     def compute(self, inputs, role):
-        return self.net(inputs["states"]), {}
+        return self.net(inputs["observations"]), {}
 
 
-# load and wrap the gymnasium environment.
-# note: the environment version may change depending on the gymnasium version
-try:
-    env = gym.make_vec("Pendulum-v1", num_envs=4, vectorization_mode="sync")
-except (gym.error.DeprecatedEnv, gym.error.VersionNotFound) as e:
-    env_id = [spec for spec in gym.envs.registry if spec.startswith("Pendulum-v")][0]
-    print("Pendulum-v1 not found. Trying {}".format(env_id))
-    env = gym.make_vec(env_id, num_envs=4, vectorization_mode="sync")
-# env.device = "cpu"
+# load the environment (note: the environment version may change depending on the gymnasium version)
+task_name = "Pendulum"
+render_mode = "human" if not args.headless else None
+env_id = [spec for spec in gym.envs.registry if spec.startswith(f"{task_name}-v")][-1]  # get latest environment version
+if args.num_envs <= 1:
+    env = gym.make(env_id, render_mode=render_mode)
+else:
+    env = gym.make_vec(env_id, num_envs=args.num_envs, render_mode=render_mode, vectorization_mode="sync")
+# wrap the environment
 env = wrap_env(env)
+
 device = env.device
 
 
@@ -82,8 +112,8 @@ memory = RandomMemory(memory_size=1024, num_envs=env.num_envs, device=device)
 # PPO requires 2 models, visit its documentation for more details
 # https://skrl.readthedocs.io/en/latest/api/agents/ppo.html#models
 models = {}
-models["policy"] = Policy(env.observation_space, env.action_space, device, clip_actions=True)
-models["value"] = Value(env.observation_space, env.action_space, device)
+models["policy"] = Policy(env.observation_space, env.state_space, env.action_space, device)
+models["value"] = Value(env.observation_space, env.state_space, env.action_space, device)
 
 
 # configure and instantiate the agent (visit its documentation to see all the options)
@@ -95,7 +125,7 @@ cfg["mini_batches"] = 32
 cfg["discount_factor"] = 0.9
 cfg["lambda"] = 0.95
 cfg["learning_rate"] = 1e-3
-cfg["learning_rate_scheduler"] = KLAdaptiveRL
+cfg["learning_rate_scheduler"] = KLAdaptiveLR
 cfg["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
 cfg["grad_norm_clip"] = 0.5
 cfg["ratio_clip"] = 0.2
@@ -105,28 +135,34 @@ cfg["entropy_loss_scale"] = 0.0
 cfg["value_loss_scale"] = 0.5
 cfg["kl_threshold"] = 0
 cfg["mixed_precision"] = True
-cfg["state_preprocessor"] = RunningStandardScaler
-cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
+cfg["observation_preprocessor"] = RunningStandardScaler
+cfg["observation_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
 cfg["value_preprocessor"] = RunningStandardScaler
 cfg["value_preprocessor_kwargs"] = {"size": 1, "device": device}
 # logging to TensorBoard and write checkpoints (in timesteps)
-cfg["experiment"]["write_interval"] = 500
-cfg["experiment"]["checkpoint_interval"] = 5000
-cfg["experiment"]["directory"] = "runs/torch/Pendulum"
+cfg["experiment"]["write_interval"] = "auto" if not args.eval else 0
+cfg["experiment"]["checkpoint_interval"] = "auto" if not args.eval else 0
+cfg["experiment"]["directory"] = f"runs/torch/{task_name}"
 
 agent = PPO(
     models=models,
     memory=memory,
     cfg=cfg,
     observation_space=env.observation_space,
+    state_space=env.state_space,
     action_space=env.action_space,
     device=device,
 )
 
 
 # configure and instantiate the RL trainer
-cfg_trainer = {"timesteps": 100000, "headless": True}
-trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=[agent])
+cfg_trainer = {"timesteps": 100000, "headless": args.headless}
+trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 
-# start training
-trainer.train()
+if args.checkpoint:
+    if not os.path.exists(args.checkpoint):
+        logger.error(f"Checkpoint file not found: '{args.checkpoint}'")
+        exit(1)
+    agent.load(args.checkpoint)
+
+trainer.train() if not args.eval else trainer.eval()

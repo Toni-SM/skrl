cf4ca98a5435f5cf066e83352ffb75e565ecbd32
Antonio Serrano Muñoz <toni.semu@gmail.com>
2025-10-11 17:40:34 -0400

    Remove test cases

--- tests/envs/wrappers/jax/test_bidexhands_envs.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from skrl.envs.wrappers.jax import BiDexHandsWrapper, wrap_env
-
-
-def test_env(capsys: pytest.CaptureFixture):
-    # check wrapper definition
-    assert isinstance(wrap_env(None, "bidexhands"), BiDexHandsWrapper)
--- tests/envs/wrappers/torch/test_bidexhands_envs.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from skrl.envs.wrappers.torch import BiDexHandsWrapper, wrap_env
-
-
-def test_env(capsys: pytest.CaptureFixture):
-    # check wrapper definition
-    assert isinstance(wrap_env(None, "bidexhands"), BiDexHandsWrapper)

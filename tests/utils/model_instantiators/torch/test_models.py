fc2e937e25ff5cafce0ba2be8ce514832cd4a805
Antonio Serrano Muñoz <toni.semu@gmail.com>
2026-05-01 23:07:40 +0200

    Fix entropy reduction

--- tests/utils/model_instantiators/torch/test_models.py
+++ tests/utils/model_instantiators/torch/test_models.py
@@ -121,6 +121,8 @@ def test_categorical_model(capsys, device):
             output = model.act(_sample_inputs(token, input_space, device))
             assert len(output) == 2
             assert output[0].shape == (10, 1)
+            entropy = model.get_entropy()
+            assert entropy.shape == (10, 1)
 
 
 @pytest.mark.parametrize("device", [None, "cpu", "cuda:0"])
@@ -146,6 +148,8 @@ def test_multicategorical_model(capsys, device):
             output = model.act(_sample_inputs(token, input_space, device))
             assert len(output) == 2
             assert output[0].shape == (10, 2)
+            entropy = model.get_entropy()
+            assert entropy.shape == (10, 1)
 
 
 @pytest.mark.parametrize("device", [None, "cpu", "cuda:0"])
@@ -201,6 +205,8 @@ def test_gaussian_model(capsys, device):
             output = model.act(_sample_inputs(token, input_space, device))
             assert len(output) == 2
             assert output[0].shape == (10, 2)
+            entropy = model.get_entropy()
+            assert entropy.shape == (10, 1)
 
 
 @pytest.mark.parametrize("device", [None, "cpu", "cuda:0"])
@@ -231,6 +237,8 @@ def test_multivariate_gaussian_model(capsys, device):
             output = model.act(_sample_inputs(token, input_space, device))
             assert len(output) == 2
             assert output[0].shape == (10, 2)
+            entropy = model.get_entropy()
+            assert entropy.shape == (10, 1)
 
 
 @pytest.mark.parametrize("single_forward_pass", [True, False])
@@ -277,6 +285,8 @@ def test_shared_gaussian_deterministic_model(capsys, device, single_forward_pass
             output = model.act(_sample_inputs(token, input_space, device), role="role_1")
             assert len(output) == 2
             assert output[0].shape == (10, 1)
+            entropy = model.get_entropy()
+            assert entropy.shape == (10, 1)
 
 
 @pytest.mark.parametrize("single_forward_pass", [True, False])
@@ -323,6 +333,8 @@ def test_shared_multivariate_gaussian_deterministic_model(capsys, device, single
             output = model.act(_sample_inputs(token, input_space, device), role="role_1")
             assert len(output) == 2
             assert output[0].shape == (10, 1)
+            entropy = model.get_entropy()
+            assert entropy.shape == (10, 1)
 
 
 @pytest.mark.parametrize("single_forward_pass", [True, False])
@@ -364,6 +376,8 @@ def test_shared_categorical_deterministic_model(capsys, device, single_forward_p
             output = model.act(_sample_inputs(token, input_space, device), role="role_1")
             assert len(output) == 2
             assert output[0].shape == (10, 1)
+            entropy = model.get_entropy()
+            assert entropy.shape == (10, 1)
 
 
 @pytest.mark.parametrize("single_forward_pass", [True, False])
@@ -405,6 +419,8 @@ def test_shared_multicategorical_deterministic_model(capsys, device, single_forw
             output = model.act(_sample_inputs(token, input_space, device), role="role_1")
             assert len(output) == 2
             assert output[0].shape == (10, 1)
+            entropy = model.get_entropy()
+            assert entropy.shape == (10, 1)
 
 
 @pytest.mark.parametrize("device", [None, "cpu", "cuda:0"])
